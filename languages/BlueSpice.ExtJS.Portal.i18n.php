c7b24ef3aeee1a18dfae0ea3609af0c0c85f04d0
Stephan Muggli <muggli@hallowelt.biz>
2014-03-28 17:20:56 +0100

    Implemented changes for getting into translatewiki
    Changed order of messages: FIRST en, SECOND qqq, AFTER THAT doesn't matter
    Removed unused lines
    
    Change-Id: I44e6a10f9d55429963aa7c1c89723b2663876ab1

--- languages/BlueSpice.ExtJS.Portal.i18n.php
+++ languages/BlueSpice.ExtJS.Portal.i18n.php
@@ -1,10 +1,21 @@
 <?php
+/**
+ * Internationalisation file for ExtJS
+ *
+ * Part of BlueSpice for MediaWiki
+ *
+ * @author     Stephan Muggli <muggli@hallowelt.biz>
+ * @package    BlueSpice_Core
+ * @subpackage ExtJS
+ * @copyright  Copyright (C) 2014 Hallo Welt! - Medienwerkstatt GmbH, All rights reserved.
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU Public License v2 or later
+ * @filesource
+ */
 
 $messages = array();
 
 $messages['en'] = array(
 	'bs-extjs-portal-config' => 'Configure portlet:',
-//unused	'bs-extjs-portal-portlets' => 'Portlets',
 	'bs-extjs-portal-title' => 'Portlet title',
 	'bs-extjs-portal-height' => 'Height',
 	'bs-extjs-portal-count' => 'Entries',
@@ -14,21 +25,8 @@ $messages['en'] = array(
 	'bs-extjs-portal-timespan-alltime' => 'All time'
 );
 
-$messages['de'] = array(
-	'bs-extjs-portal-config' => 'Portlet konfigurieren:',
-//unused	'bs-extjs-portal-portlets' => 'Portlets',
-	'bs-extjs-portal-title' => 'Portlettitel',
-	'bs-extjs-portal-height' => 'Höhe',
-	'bs-extjs-portal-count' => 'Anzahl',
-	'bs-extjs-portal-timespan' => 'Zeitraum',
-	'bs-extjs-portal-timespan-week' => 'Woche',
-	'bs-extjs-portal-timespan-month' => 'Monat',
-	'bs-extjs-portal-timespan-alltime' => 'Unbegrenzt'
-);
-
 $messages['qqq'] = array(
 	'bs-extjs-portal-config' => 'Title of configuration dialogues for dashboard portlets. Followed by the name of the portlet, hence the colon.',
-//unused	'bs-extjs-portal-portlets' => 'Portlets',
 	'bs-extjs-portal-title' => 'Label for title input field',
 	'bs-extjs-portal-height' => 'Label for height input field',
 	'bs-extjs-portal-count' => 'Label for input field that counts number of items shown',
@@ -36,4 +34,15 @@ $messages['qqq'] = array(
 	'bs-extjs-portal-timespan-week' => 'Label for timespan option "week"',
 	'bs-extjs-portal-timespan-month' => 'Label for timespan option "month"',
 	'bs-extjs-portal-timespan-alltime' => 'Label for timespan option "unlimited"'
+);
+
+$messages['de'] = array(
+	'bs-extjs-portal-config' => 'Portlet konfigurieren:',
+	'bs-extjs-portal-title' => 'Portlettitel',
+	'bs-extjs-portal-height' => 'Höhe',
+	'bs-extjs-portal-count' => 'Anzahl',
+	'bs-extjs-portal-timespan' => 'Zeitraum',
+	'bs-extjs-portal-timespan-week' => 'Woche',
+	'bs-extjs-portal-timespan-month' => 'Monat',
+	'bs-extjs-portal-timespan-alltime' => 'Unbegrenzt'
 );
