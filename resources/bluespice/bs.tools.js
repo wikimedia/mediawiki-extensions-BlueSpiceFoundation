7fa9e19f1a7bf0f2fdf595084b9f9070cb9def5f
Robert Vogel <vogel@hallowelt.biz>
2014-07-15 08:51:15 +0200

    Rework of tooltip mechanism
    CSS is skin independent
    Now using ExtJS
    
    Change-Id: I81ceb1d7e8baa9f85629aaa5bd6a04ed9843b788

--- resources/bluespice/bs.tools.js
+++ resources/bluespice/bs.tools.js
@@ -1,33 +1,19 @@
-BsTools = {
-	tooltipInit: function(){
-		var target = $(this).next().find('#' + $(this).attr('id') + '-target');
-		$(this).next().offset({ left: $(this).offset().left });
+$(function(){
+	$('.bs-tooltip-link').each(function(){
 
-		$(this).hover( function() {
-			$(this).next().stop( true, true ).slideDown();
-		}, function() {
-			$(this).next().stop( true, true ).delay(1000).slideUp();
-		});
-		$(this).next().hover( function() {
-			$(this).stop( true, true ).slideDown();
-		}, function() {
-			$(this).stop( true, true ).delay(1000).slideUp();
-		});
+		var conf = {
+			target: this,
+			title: $(this).data('bs-tt-title') || '&#160;',
+			html: $(this).data('bs-tt-html') || '',
+			contentEl: $(this).data('bs-tt-target') || document,
+			anchor: $(this).data('bs-tt-anchor') || 'top',
+			autoHide: $(this).data('bs-tt-autohide') || false,
+			maxHeight: $(this).data('bs-tt-maxheight'),
+			minWidth: $(this).data('bs-tt-maxheight') || 100,
+			overflowX: 'auto',
+			overflowY: 'auto'
+		};
 
-		if( typeof target.attr('data-maxheight') === 'undefined' ) return;
-		//items with display none have no height :(
-		//if( target.height() < target.attr('data-maxheight') ) return;
-
-		target.slimScroll({
-			height: target.attr('data-maxheight') + 'px',
-			color: '#3F527F',
-			alwaysVisible: true,
-			railVisible: true
-		});
-	}
-}
-
-//Tooltips
-$.fn.bstooltip = function(){
-	$(this).each( BsTools.tooltipInit );
-};
+		Ext.create('Ext.tip.ToolTip', conf);
+	});
+});
