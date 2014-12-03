48b0084fb321dd662966d94c3ebfc76398bc5d03
Robert Vogel <vogel@hallowelt.biz>
2014-12-03 12:29:41 +0100

    Added new Maintenance base classes
    * Also removed legacy Code
    
    Change-Id: Ib49d6a6375204514eeac2b44517d157641ef8422

---
+++ maintenance/BSBatchFileProcessorBase.php
@@ -0,0 +1,112 @@
+<?php
+
+require_once( __DIR_.'/BSMaintenance.php' );
+
+abstract class BSBatchFileProcessorBase extends BSMaintenance {
+
+	protected $sSrc = '';
+	protected $sDest = __DIR__;
+	protected $oCurrentFile = null;
+	protected $aFiles = array();
+	protected $aFileExtensionWhitelist = array();
+	protected $aErrors = array();
+
+	public function __construct() {
+		parent::__construct();
+
+		$this->addOption('src', 'The path to the source directory', false, true);
+		$this->addOption('dest', 'The path to the destination directory', false, true);
+
+		$this->aFileExtensionWhitelist = array_map(
+			'strtoupper', $this->aFileExtensionWhitelist
+		);
+	}
+
+	public function execute() {
+		//wfCountDown( 5 );
+
+		$this->sSrc = $this->getOption( 'src', $this->sSrc );
+		$this->sDest = $this->getOption( 'dest', $this->sDest );
+
+		$aFiles = $this->getFileList();
+
+		$iProcessedFiles = 0;
+		foreach( $aFiles as $sFileName => $oFile ) {
+			if( $oFile instanceof SplFileInfo !== true ) {
+				$this->error( 'Could not process list item: '
+						. $sFileName . ' '
+						. var_export( $oFile, true )
+				);
+				continue;
+			}
+			$this->output( 'Processing ' . $oFile->getPathname().' ...' );
+			$mResult = $this->processFile( $oFile );
+			if ( $mResult !== true ) {
+				$this->error( '... error:' . $mResult );
+			}
+			else {
+				$this->output( '... done.' );
+				$iProcessedFiles++;
+			}
+		}
+
+		$this->output( $iProcessedFiles.' file(s) processed.' );
+		$this->output( count($this->aErrors).' errors(s) occured.' );
+		if( count( $this->aErrors ) > 0 ) {
+			$this->output(
+				implode( "\n", $this->aErrors )
+			);
+		}
+	}
+
+	/**
+	 * Throw an error to the user. Doesn't respect --quiet, so don't use
+	 * this for non-error output
+	 *
+	 * BSMaintenance: Public to allow hook callbacks to write output.
+	 * @param $err String: the error to display
+	 * @param $die Int: if > 0, go ahead and die out using this int as the code
+	 */
+	public function error( $err, $die = 0 ) {
+		$this->aErrors[] = $err;
+		parent::error( $err, $die );
+	}
+
+	/**
+	 *
+	 * @return array\RecursiveIteratorIterator
+	 */
+	public function getFileList() {
+		$sPath = realpath( $this->sSrc );
+		$this->output( 'Fetching file list from "'.$sPath.'" ...' );
+
+		$oFiles = new RecursiveIteratorIterator(
+			new RecursiveDirectoryIterator( $sPath ),
+			RecursiveIteratorIterator::SELF_FIRST
+		);
+
+		$aFiles = array();
+		foreach ( $oFiles as $sPath => $oFile ) {
+			if( $oFile instanceof SplFileInfo === false ) {
+				$this->error( 'Not a valid SplFileInfo object: ' . $sPath );
+			}
+			if( !empty( $this->aFileExtensionWhitelist ) ) {
+				$sFileExt = strtoupper( $oFile->getExtension() );
+				if( !in_array( $sFileExt, $this->aFileExtensionWhitelist ) ) {
+					continue;
+				}
+			}
+			$aFiles[$oFile->getFilename()] = $oFile;
+		}
+
+		ksort($aFiles, SORT_NATURAL);
+		$this->output( '... found ' . count( $aFiles ) . ' file(s).' );
+		return $aFiles;
+	}
+
+	/**
+	 *
+	 * @param SplFileInfo $oFile
+	 */
+	public abstract function processFile($oFile);
+}
---
+++ maintenance/BSRemoteAPIBase.php
@@ -0,0 +1,141 @@
+<?php
+
+require_once( __DIR__.'/BSMaintenance.php' );
+
+class BSRemoteAPIBase extends BSMaintenance {
+
+	public function __construct() {
+		parent::__construct();
+
+		$this->addOption('targetapi', 'Absolute path the target wiki\'s "api.php"', true, true);
+		$this->addOption('u', 'A valid username on the target wiki with sufficient write permissions', true, true);
+		$this->addOption('p', 'The users password for API login. If not provided as argument you will be promted for it', false, true);
+	}
+
+	protected $apiUrl = '';
+	protected $username = '';
+	protected $password = '';
+	protected $config = '';
+	protected $configArray = null;
+	protected $cookieJar = null;
+	protected $token = null;
+	protected $edittoken = null;
+
+	public function execute() {
+		$this->apiUrl = $this->getOption( 'targetapi' );
+		$this->username = $this->getOption( 'u' );
+		if( empty($this->username) ) {
+			$this->error( '"username" can not be empty');
+			return;
+		}
+		$this->password = $this->getOption( 'p' );
+		if( $this->password === null ) {
+			$this->password = $this->readconsole('Password for user "'.$this->username.'": ');
+		}
+		if( empty($this->password) ) {
+			$this->error( '"password" can not be empty');
+			return;
+		}
+
+		if( !$this->doAPILogin() ) {
+			$this->error( 'Authentication failed' );
+			return;
+		}
+		$this->output( 'Authentication successfull' );
+
+		$this->getAPIEditToken();
+	}
+
+	public function doAPILogin() {
+		$options = array(
+			'method' => 'POST',
+			'postData' => array(
+				'action' => 'login',
+				'format' => 'json',
+				'lgname' => $this->username,
+				'lgpassword' => $this->password
+			)
+		);
+
+		//Second pass
+		if ( $this->token !== null ) {
+			$options['postData']['lgtoken'] = $this->token;
+		}
+
+		Http::$httpEngine = 'curl';
+		$req = MWHttpRequest::factory( $this->apiUrl, $options );
+
+		if ( $this->cookieJar !== null ) {
+			$req->setCookieJar( $this->cookieJar );
+		}
+
+		$status = $req->execute();
+
+		if ( $status->isOK() ) {
+			$response = FormatJson::decode( $req->getContent() );
+
+			if ( isset( $response->login ) && isset( $response->login->result ) ) {
+
+				if ( strtolower( $response->login->result ) === 'needtoken' ) {
+					$this->token = $response->login->token;
+					$this->cookieJar = $req->getCookieJar();
+					return $this->doAPILogin();
+				}
+				elseif ( strtolower ( $response->login->result ) === 'success' ) {
+					$this->cookieJar = $req->getCookieJar();
+					return true;
+				}
+			}
+
+			return false;
+		}
+		return false;
+	}
+
+	public function makePOSTRequest( $aOptions ) {
+		$options = array(
+			'method' => 'POST',
+			'postData' => $aOptions
+		);
+
+		Http::$httpEngine = 'curl';
+		$request = MWHttpRequest::factory( $this->apiUrl, $options );
+		$request->setCookieJar( $this->cookieJar );
+
+		return $request;
+	}
+
+	public function getAPIEditToken() {
+		$query = array(
+			'action' => 'query',
+			'format' => 'json',
+			'meta' => 'tokens',
+			'type' => 'csrf',
+		);
+
+		Http::$httpEngine = 'curl';
+		$req = MWHttpRequest::factory(
+			wfAppendQuery($this->apiUrl, $query)
+		);
+		$req->setCookieJar( $this->cookieJar );
+
+		$status = $req->execute();
+
+		if ( $status->isOK() ) {
+			$response = FormatJson::decode( $req->getContent() );
+			if( isset( $response->query ) && isset( $response->query->tokens ) && isset( $response->query->tokens->csrftoken ) ) {
+				$this->edittoken = $response->query->tokens->csrftoken;
+				return true;
+			}
+		}
+
+		return false;
+	}
+}
+
+$maintClass = 'BSRemoteAPIBase';
+if (defined('RUN_MAINTENANCE_IF_MAIN')) {
+	require_once( RUN_MAINTENANCE_IF_MAIN );
+} else {
+	require_once( DO_MAINTENANCE ); # Make this work on versions before 1.17
+}
