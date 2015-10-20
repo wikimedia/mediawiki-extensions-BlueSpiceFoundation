38f8bcb2a5fa779a5fdc28eab23fb14396447f4b
Robert Vogel <vogel@hallowelt.biz>
2015-10-20 10:05:51 +0200

    maintenance: Added user im- and export scripts
    There are many cases where customers want to pre populate the user
    database of a new wiki. E.g. when migrating from another system. Here are
    two scripts that can ease the process.
    
    Needs merge to REL1_23
    
    Change-Id: Id5069939902e59a936bbc24d5e83df67ae7d69a4

---
+++ maintenance/BSImportUsers.php
@@ -0,0 +1,121 @@
+<?php
+
+require_once( 'BSMaintenance.php' );
+
+class BSImportUsers extends BSMaintenance {
+	public function __construct() {
+		$this->addOption('src', 'The path to the source file', true, true);
+		$this->addOption('defaultpw', 'A password that should be set for any new user', false, true);
+		$this->addOption('createuserpage', 'Wether or not a user page should be created (<userpage> element needs to be available)', false, false);
+
+		parent::__construct();
+	}
+
+	public function execute() {
+		$oDOM = new DOMDocument();
+		$oDOM->load( $this->getOption( 'src' ) );
+		$oDOM->recover = true;
+
+		$oUserNodes = $oDOM->getElementsByTagName( 'user' );
+		foreach ( $oUserNodes as $oUserNode ) {
+			$sUserName = $this->getChildNodeValue( $oUserNode, 'name' );
+			$oUser = User::newFromName( $sUserName );
+			if( $oUser instanceof User === false ) {
+				$this->output( $sUserName.' is not a valid username' );
+				continue;
+			}
+
+			if( $oUser->getId() !== 0 ) {
+				$this->output( $oUser->getName().'already exists. UserID: '.$oUser->getId() );
+				$this->output( 'Skipping!' ); //TODO: make optional
+				continue;
+			}
+
+			$sUserRealName = $this->getChildNodeValue( $oUserNode, 'realname' );
+			if( !empty( $sUserRealName ) ) {
+				$oUser->setRealName( $sUserRealName );
+			}
+
+			$sUserEmail = $this->getChildNodeValue( $oUserNode, 'email' );
+			if( !empty( $sUserEmail ) ) {
+				$oUser->setEmail( $sUserEmail );
+			}
+
+			$sUserPassword = $this->getOption( 'defaultpw', '' );
+			if( !empty ( $sUserPassword ) ) {
+				$oUser->setPassword( $sUserPassword );
+			}
+
+			//TODO: maybe write 'touched', 'registration', etc. directly to DB?
+
+			$oProperties = $oUserNode->getElementsByTagName('property');
+			foreach( $oProperties as $oProperty ) {
+				$oUser->setOption(
+					$oProperty->getAttribute( 'name' ),
+					$oProperty->getAttribute( 'value' )
+				);
+			}
+
+			$oStatus = $oUser->addToDatabase();
+			if( $oStatus->isOK() ) {
+				$this->output( $oUser->getName().' successfully added to database. UserID: '.$oUser->getId() );
+			}
+			else {
+				$this->error( $oUser->getName().' could not be added to database. Message '.$oStatus->getMessage()->plain() );
+				continue;
+			}
+
+			$oGroups = $oUserNode->getElementsByTagName('group');
+			foreach( $oGroups as $oGroup ) {
+				$oUser->addGroup( $oGroup->getAttribute( 'name' ) );
+			}
+
+			if( $this->getOption( 'createuserpage', false ) ) {
+				if( $oUser->getUserPage()->exists() ) {
+					continue;
+				}
+				$sContent = $this->getChildNodeValue( $oUserNode, 'userpage' );
+				if( empty( $sContent ) ) {
+					continue;
+				}
+
+				$oContent = ContentHandler::makeContent(
+					$sContent,
+					$oUser->getUserPage()
+				);
+
+				$oWikiPage = WikiPage::factory( $oUser->getUserPage() );
+				$oEditStatus = $oWikiPage->doEditContent( $oContent, __CLASS__ );
+				if( $oEditStatus->isOK() ) {
+					$this->output( 'Page '.$oUser->getUserPage()->getPrefixedText().' successfully created.' );
+				}
+				else {
+					$this->error( 'Page '.$oUser->getUserPage()->getPrefixedText().' could not be created. Message: '.$oEditStatus->getMessage()->plain() );
+				}
+			}
+		}
+	}
+
+	/**
+	 *
+	 * @param DOMElement $oNode
+	 * @param string $param1
+	 * @return string
+	 */
+	public function getChildNodeValue( $oNode, $sChildNodeName ) {
+		$oChildNode = $oNode->getElementsByTagName( $sChildNodeName )->item( 0 );
+		if( $oChildNode instanceof DOMElement === false ) {
+			return '';
+		}
+
+		return $oChildNode->nodeValue;
+	}
+
+}
+
+$maintClass = 'BSImportUsers';
+if (defined('RUN_MAINTENANCE_IF_MAIN')) {
+	require_once( RUN_MAINTENANCE_IF_MAIN );
+} else {
+	require_once( DO_MAINTENANCE ); # Make this work on versions before 1.17
+}
