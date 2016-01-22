6139f01cad9a556a946a88a7bda32fa265a1628d
Robert Vogel <vogel@hallowelt.biz>
2016-01-22 16:03:04 +0100

    Added BS.store.BSApi
    Added base class that lets a developer easily access the API as a source
    of an ExtJS data consumer
    
    Change-Id: I554271712a08248173e7c1f4070bbe1c43c9b9b6

---
+++ resources/bluespice.extjs/BS/store/BSApi.js
@@ -0,0 +1,30 @@
+Ext.define('BS.store.BSApi', {
+	extend: 'Ext.data.JsonStore',
+	apiAction: null,
+
+	constructor: function( cfg ) {
+		cfg = Ext.apply({
+			proxy: {
+				type: 'ajax',
+				url: mw.util.wikiScript('api'),
+				extraParams: {
+					format: 'json'
+				},
+				reader: {
+					type: 'json',
+					root: 'results',
+					idProperty: 'id',
+					totalProperty: 'total'
+				}
+			},
+			autoLoad: true,
+			remoteSort: true,
+			sortInfo: {
+				field: 'id',
+				direction: 'ASC'
+			}
+		}, cfg);
+		cfg.proxy.extraParams.action = cfg.apiAction;
+		this.callParent([cfg]);
+	}
+});
