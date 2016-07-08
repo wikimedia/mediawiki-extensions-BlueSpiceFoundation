66eb5b5649410f9451a2b7e38f9b86a6c183e9ee
Robert Vogel <vogel@hallowelt.biz>
2016-07-08 14:52:58 +0200

    Fix for empty grid pages
    ExtJS Grid/PagingToolbar: a user is on the last page of results
    (bigger than "1") and deletes all entries from it.
    When the store reloads with the old page info and sees an empty
    result set it thinks there are no further results to display, even
    if the 'totalProperty' says there are more. It just displays
    "0 entries" to the user, leaving him without the change to select
    a previous page.
    Therefore we check for these circumstances and select a previous
    page if possible
    
    See ERM3303 for details
    
    --- NEEDS CHERRYPICK TO MASTER ---
    
    Change-Id: I0eb41ef31f8acdc58671c8623520f2804883736b

--- resources/bluespice.extjs/BS/store/BSApi.js
+++ resources/bluespice.extjs/BS/store/BSApi.js
@@ -26,5 +26,32 @@ Ext.define('BS.store.BSApi', {
 		}, cfg);
 		cfg.proxy.extraParams.action = cfg.apiAction;
 		this.callParent([cfg]);
+		this.on( 'load', this.checkForEmptyPage, this );
+	},
+
+	/**
+	 * Checks whether the current page is empty and loads previous one if
+	 * possible
+	 * @param BS.store.BSApi sender
+	 * @param Ext.data.Model[] records
+	 * @param Boolean successful
+	 * @param Object eOpts
+	 * @returns void
+	 */
+	checkForEmptyPage: function( sender, records, successful, eOpts ) {
+		/**
+		 * ExtJS Grid/PagingToolbar: a user is on the last page of results
+		 * (bigger than "1") and deletes all entries from it.
+		 * When the store reloads with the old page info and sees an empty
+		 * result set it thinks there are no further results to display, even
+		 * if the 'totalProperty' says there are more. It just displays
+		 * "0 entries" to the user, leaving him without the change to select
+		 * a previous page.
+		 * Therefore we check for these circumstances and select a previous
+		 * page if possible
+		 */
+		if( records.length === 0 && sender.getTotalCount() !== 0 && sender.currentPage !== 1 ) {
+			sender.loadPage( sender.currentPage - 1 );
+		}
 	}
 });
