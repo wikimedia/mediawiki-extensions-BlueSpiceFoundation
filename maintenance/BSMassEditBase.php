b0dbf5534bbf59c2460b0306fccd934980648339
Robert Vogel <vogel@hallowelt.biz>
2016-08-01 12:43:20 +0200

    Added new maintenance script base class
    Mass edit is a very common task. This base class should make it easier to
    develop such sripts.
    
    NEEDS CHERRY-PICK TO MASTER
    
    Change-Id: I8ec194feea638faeee82eac8309036382724f3d8
    (cherry picked from commit a9520bb19baab50a8a97b100d0ac464f1be64211)

---
+++ maintenance/BSMassEditBase.php
@@ -0,0 +1,165 @@
+<?php
+
+require_once( __DIR__.'/BSMaintenance.php' );
+
+class BSMassEditBase extends BSMaintenance {
+	protected $aFileMap = array();
+	protected $aPageNameMap = array();
+
+	protected $iTitleCount = 0;
+	protected $iModifiedTitleCount = 0;
+	protected $iFailureTitleCount = 0;
+
+	public function __construct() {
+		parent::__construct();
+		$this->addOption( 'dry', 'Do not really modify contents' );
+		$this->addOption( 'output', 'Save outputs to file', false, true );
+	}
+
+	public function execute() {
+		$aTitles = $this->getTitleList();
+		$this->iTitleCount = count( $aTitles );
+
+		foreach( $aTitles as $sTitle => $oTitle ) {
+			if( $oTitle instanceof Title === false ) {
+				$this->error( "Invalid Title '$sTitle'!" );
+				continue;
+			}
+			$oWikiPage = WikiPage::factory( $oTitle );
+			$this->output( "\nModifying '{$oTitle->getPrefixedText()}'..." );
+			$oNewContent = $this->modifyContent( $oWikiPage->getContent(), $oWikiPage );
+			if( $oNewContent instanceof Content === false ) {
+				$this->output( "--> Content of page '{$oTitle->getPrefixedText()}' has NOT been modified!" );
+				continue;
+			}
+
+			if( $this->isDryMode() ) {
+				continue;
+			}
+
+			$oStatus = $oWikiPage->doEditContent(
+				$oNewContent,
+				$this->getEditSummay( $oWikiPage )
+			);
+			if( !$oStatus->isOK() ) {
+				$this->error( "--> Content of page {$oTitle->getPrefixedText()} could not be modified: {$oStatus->getMessage()->plain()}");
+				$this->iFailureTitleCount++;
+			}
+			else {
+				$this->output( "--> Content of page {$oTitle->getPrefixedText()} has successfully been modified" );
+				$this->iModifiedTitleCount++;
+			}
+		}
+
+		$this->output( 'Processed Titles: '.$this->iTitleCount );
+		$this->output( 'Modified contents: '.$this->iModifiedTitleCount );
+		$this->output( 'Failed modifications: '.$this->iFailureTitleCount );
+	}
+
+	/**
+	 * Provides all the titles the edit should be performed on
+	 * @return array of Title objects
+	 */
+	protected function getTitleList() {
+		$dbr = $this->getDB( DB_SLAVE );
+		$aTitles = array();
+
+		$res = $dbr->select( 'page', '*' );
+		foreach( $res as $row ) {
+			$oTitle = Title::newFromRow( $row );
+			$aTitles[ $oTitle->getPrefixedDBkey() ] = $oTitle;
+		}
+
+		return $aTitles;
+	}
+
+	public function isDryMode() {
+		return (bool)$this->getOption( 'dry', false );
+	}
+
+	/**
+	 * Can be overwritten by subclass if other content model than WikiText should be processed
+	 * @param Content $oContent
+	 * @param WikiPage $oWikiPage
+	 * @return Content
+	 */
+	protected function modifyContent( $oContent, $oWikiPage ) {
+		$sTextContent = $oContent->getContentHandler()->getContentText( $oContent );
+		$sNewTextContent = $this->modifyTextContent(
+			$sTextContent,
+			$oWikiPage
+		);
+
+		if( $sTextContent === $sNewTextContent ) {
+			return null; //No change? No edit!
+		}
+
+		return $oWikiPage->getContentHandler()->makeContent(
+			$sNewTextContent,
+			$oWikiPage->getTitle()
+		);
+	}
+
+	/**
+	 * Default method that gets called for actual modification of text
+	 * repesenation of a wikipage content
+	 * Can be overwritten by subclass if other than preg_replace_callback
+	 * implementation should be used
+	 * @param string $sContent
+	 * @param WikiPage $oWikiPage
+	 * @return string
+	 */
+	protected function modifyTextContent( $sContent, $oWikiPage ) {
+		return preg_replace_callback(
+			$this->getTextModificationRegexPatterns(),
+			array( $this, 'textModificationCallback' ),
+			$sContent
+		);
+	}
+
+	/**
+	 * To have a nice summary in revision history for the edit made
+	 * @param WikiPage $oWikiPage
+	 * @return string
+	 */
+	protected function getEditSummay( $oWikiPage ) {
+		return "Modified by script '".__CLASS__."'";
+	}
+
+	public function output($out, $channel = null) {
+		$sOutFile = $this->getOption( 'output', false );
+		if( $sOutFile ) {
+			file_put_contents( $sOutFile, "$out\n", FILE_APPEND );
+		}
+		parent::output($out, $channel);
+	}
+
+	public function error($err, $die = 0) {
+		$sOutFile = $this->getOption( 'output', false );
+		if( $sOutFile ) {
+			file_put_contents( $sOutFile, "$err\n", FILE_APPEND );
+		}
+		parent::error($err, $die);
+	}
+
+	/**
+	 * Provides the regex for default implementation of "modifyTextContent"
+	 * @return mixed string or array-of-strings containig a valid regular expression
+	 */
+	protected function getTextModificationRegexPatterns() {
+		//Just an example: "Find all internal links in WikiText"
+		return '#\[\[(.*?)\]\]#s';
+	}
+
+	/**
+	 * Default callback for replacements based on
+	 * 'getTextModificationRegexPatterns'
+	 * Should probably be overwritten by subclass
+	 * @param type $aMatches
+	 * @return type
+	 */
+	public function textModificationCallback( $aMatches ) {
+		//Just a dummy: returns unmodified
+		return $aMatches[0];
+	}
+}
