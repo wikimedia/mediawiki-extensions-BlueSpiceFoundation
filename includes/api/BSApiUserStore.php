7de7e1700600bb5bb82aee8737d64443ff9954b9
mglaser <glaser@hallowelt.biz>
2016-08-18 23:59:00 +0200

    UserStore: include blocked information and hide blocked users
    There is now information about the block status of users. Blocked
    users are not shown (per default filter), unless the default filter is
    overridden. This can currently only be done in UserManager (per
    corresponding commit)
    
    Patch Set 2: As per Robert: integrated "enabled" filter with others
    
    Change-Id: I8d3ec0e6224878ae1b4837e499c874628d0bc35b

--- includes/api/BSApiUserStore.php
+++ includes/api/BSApiUserStore.php
@@ -2,16 +2,25 @@
 
 class BSApiUserStore extends BSApiExtJSStoreBase {
 
+	protected $aGroups = array();
+	protected $aBlocks = array();
+
 	protected function makeData($sQuery = '') {
 		$dbr = $this->getDB();
 
-		$aGroups = array();
+		$this->aGroups = array();
 		$groupsRes = $dbr->select( 'user_groups', '*' );
 		foreach( $groupsRes as $row ) {
-			if( !isset( $aGroups[$row->ug_user] ) ) {
-				$aGroups[$row->ug_user] = array();
+			if( !isset( $this->aGroups[$row->ug_user] ) ) {
+				$this->aGroups[$row->ug_user] = array();
 			}
-			$aGroups[$row->ug_user][] = $row->ug_group;
+			$this->aGroups[$row->ug_user][] = $row->ug_group;
+		}
+
+		$this->aBlocks = array();
+		$blocksRes = $dbr->select( 'ipblocks', '*' );
+		foreach( $blocksRes as $row ) {
+			$this->aBlocks[$row->ipb_user] = $row->ipb_address;
 		}
 
 		//TODO: It would be very cool to have the permissions as a filterable
@@ -23,7 +32,7 @@ class BSApiUserStore extends BSApiExtJSStoreBase {
 		$aData = array();
 		$userRes = $dbr->select( 'user', '*' );
 		foreach( $userRes as $aRow ) {
-			$aResRow = $this->makeResultRow( $aRow, $aGroups );
+			$aResRow = $this->makeResultRow( $aRow );
 			if( !$aResRow ) {
 				continue;
 			}
@@ -36,10 +45,9 @@ class BSApiUserStore extends BSApiExtJSStoreBase {
 	/**
 	 * Builds a single result set
 	 * @param stdClass $row
-	 * @param array $aGroups
 	 * @return array
 	 */
-	protected function makeResultRow( $row, $aGroups = array() ) {
+	protected function makeResultRow( $row ) {
 		$oUserPageTitle = Title::makeTitle( NS_USER, $row->user_name );
 		return array(
 			'user_id' => (int) $row->user_id,
@@ -47,12 +55,47 @@ class BSApiUserStore extends BSApiExtJSStoreBase {
 			'user_real_name' => $row->user_real_name,
 			'user_registration' => $row->user_registration,
 			'user_editcount' => (int) $row->user_editcount,
-			'groups' => isset( $aGroups[$row->user_id] ) ? $aGroups[$row->user_id] : array(),
+			'groups' => isset( $this->aGroups[$row->user_id] ) ? $this->aGroups[$row->user_id] : array(),
+			'enabled' => isset( $this->aBlocks[$row->user_id] ) ? false : true,
 			'page_link' => Linker::link( $oUserPageTitle, $row->user_name.' ' ), //The whitespace is to aviod automatic rewrite to user_real_name by BSF
 
 			//legacy fields
 			'display_name' => $row->user_real_name == null ? $row->user_name : $row->user_real_name,
 			'page_prefixed_text' => $oUserPageTitle->getPrefixedText()
 		);
+	}
+
+	/**
+	 * Set "enabled" filter to true as a default. This needs to be done
+	 * programmatically, because the filter json must be integrated with
+	 * other filters.
+	 *
+	 * @param string $paramName Parameter name
+	 * @param array|mixed $paramSettings Default value or an array of settings
+	 *  using PARAM_* constants.
+	 * @param bool $parseLimit Parse limit?
+	 * @return mixed Parameter value
+	 */
+	protected function getParameterFromSettings( $paramName, $paramSettings, $parseLimit ) {
+		$value = parent::getParameterFromSettings( $paramName, $paramSettings, $parseLimit );
+
+		if ( $paramName === 'filter' ) {
+
+			$bEnabledFilterIsSet = false;
+			foreach ( $value as $filter ) {
+				if ( $filter->field == 'enabled' ) {
+					$bEnabledFilterIsSet = true;
+				}
+			}
+			if ( !$bEnabledFilterIsSet ) {
+				$value[] = (object) array(
+					'type' => 'boolean',
+					'value' => true,
+					'field' => 'enabled'
+				);
+			}
+		}
+
+		return $value;
 	}
 }
