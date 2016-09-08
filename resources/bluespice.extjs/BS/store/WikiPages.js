a524f9a265408fa05441db4b527772f3c9902d60
mglaser <glaser@hallowelt.com>
2016-09-08 10:13:13 +0200

    ExtJS/API deleted superfluous file.
    Change-Id: Ibf6611f37cd13e421af7ff02c314816e157f2c4c

--- resources/bluespice.extjs/BS/store/WikiPages.js
+++
@@ -1,8 +0,0 @@
-Ext.define('AM.store.Users', {
-	extend: 'Ext.data.Store',
-	fields: ['name', 'email'],
-	data: [
-		{name: 'Ed',    email: 'ed@sencha.com'},
-		{name: 'Tommy', email: 'tommy@sencha.com'}
-	]
-});
