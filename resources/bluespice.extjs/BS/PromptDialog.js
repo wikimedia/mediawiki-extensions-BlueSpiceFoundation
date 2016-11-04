47b72016cc5a12cc84e30cc8409657f031af4252
Leonid Verhovskij <l.verhovskij@gmail.com>
2016-11-04 14:05:36 +0100

    improve autofocus in extjs prompt dialog
    Change-Id: Ifdea378454ae70542002ef69756a746edbc08221

--- resources/bluespice.extjs/BS/PromptDialog.js
+++ resources/bluespice.extjs/BS/PromptDialog.js
@@ -3,17 +3,26 @@ Ext.define( 'BS.PromptDialog', {
 
 	afterInitComponent: function() {
 		this.callParent( arguments );
-		
+
 		this.btnOK.setText( mw.message('bs-extjs-ok').plain() );
 		this.btnCancel.setText( mw.message('bs-extjs-cancel').plain() );
-		
+
 		this.tfPrompt = Ext.create( 'Ext.form.TextField', {
 			width: '100%'
 		} );
-		
+
 		this.cntMain.add(this.tfPrompt);
+		this.on( 'show', function() {
+			this.tfPrompt.focus();
+		}, this );
+
+		this.tfPrompt.on( 'specialkey', function(field, e){
+			if ( e.getKey() === e.ENTER ) {
+				this.btnOK.fireEvent( 'click', this );
+			}
+		}, this );
 	},
-	
+
 	btnOKClicked: function() {
 		var value = this.tfPrompt.getValue();
 		this.fireEvent( 'ok', { value: value } );
