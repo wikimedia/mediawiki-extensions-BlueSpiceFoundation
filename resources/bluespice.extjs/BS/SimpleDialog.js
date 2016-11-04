f28309d0b88131555e7f56fecc93b3d561d8b45d
Robert Vogel <vogel@hallowelt.biz>
2016-11-04 15:12:28 +0100

    Reenable closeAction: 'destroy'
    No idea why this ever got disabled. Commit logs don't hold any useful
    comment about it.
    
    Reenabling it for testing.
    
    Change-Id: Ib1ab2da60b89292b7363578d4df94355be872cee

--- resources/bluespice.extjs/BS/SimpleDialog.js
+++ resources/bluespice.extjs/BS/SimpleDialog.js
@@ -6,7 +6,7 @@ Ext.define( 'BS.SimpleDialog', {
 	width: 300,
 	minHeight: 100,
 	modal:true,
-	//closeAction: 'destroy',
+	closeAction: 'destroy',
 	buttonAlign: 'center',
 
 	//Custom Settings
@@ -16,44 +16,44 @@ Ext.define( 'BS.SimpleDialog', {
 		if( this.titleMsg ) {
 			this.setTitle( mw.message(this.titleMsg).plain() );
 		}
-		
+
 		this.cntMain = Ext.create('Ext.Container',{
 			id: this.idPrefix + '-cnt-main',
 			padding: 5
 		});
-		
+
 		this.items = [
 			this.cntMain
 		];
-		
+
 		this.btnOK = Ext.create('Ext.Button',{
 			text: mw.message('bs-extjs-ok').plain(),
 			id: this.idPrefix + '-btn-ok'
 		});
 		this.btnOK.on( 'click', this.btnOKClicked, this );
-		
+
 		this.btnCancel = Ext.create('Ext.Button',{
 			text: mw.message('bs-extjs-cancel').plain(),
 			id: this.idPrefix + '-btn-cancel'
 		});
 		this.btnCancel.on( 'click', this.btnCancelClicked, this );
-		
+
 		this.addEvents( 'ok', 'cancel' );
 
 		this.afterInitComponent(arguments);
 		this.callParent( arguments );
 	},
-	
+
 	afterInitComponent: function () {
-		
+
 	},
-	
+
 	btnOKClicked: function() {
 		this.fireEvent( 'ok' );
 		this.close();
 	},
-	
-		
+
+
 	btnCancelClicked: function() {
 		this.fireEvent( 'cancel' );
 		this.close();
