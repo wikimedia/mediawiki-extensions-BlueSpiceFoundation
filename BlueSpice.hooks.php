6ce0b4809c1180d9a0b9fa6ebb162bc6d2ebe542
Robert Vogel <vogel@hallowelt.biz>
2016-11-21 15:21:56 +0100

    Clean up old unused code #2
    Oracle support has been dropped some time ago.
    
    Change-Id: I0c063184b605273515dc50732312f75bf37db154

--- BlueSpice.hooks.php
+++ BlueSpice.hooks.php
@@ -4,11 +4,3 @@
 // START cache invalidation hooks
 $wgHooks['PageContentSaveComplete'][] = 'BsCacheHelper::onPageContentSaveComplete';
 // END cache invalidation hooks
-
-if ( !isset( $wgHooks['EditPage::showEditForm:initial'] ) ) {
-	$wgHooks['EditPage::showEditForm:initial'] = array();
-}
-
-if ( $wgDBtype == 'oracle' ) {
-	$wgHooks['ArticleDelete'][] = 'BSOracleHooks::onArticleDelete';
-}
