1ab8a65a4c0187773721b9afb20b8585a4f78688
mglaser <glaser@hallowelt.com>
2017-04-27 12:04:22 +0200

    Maintenance script to adopt unittest log to jenkins
    Jenkins wants a very special format for test case grouping. This
    script taks the phpunit log-junit output and modifies it
    accordingly
    
    Change-Id: Ib17dc5507f44cc32100d4c7419849fe2f44a0f4f

---
+++ maintenance/PimpUnitTestResultsForJenkins.php
@@ -0,0 +1,62 @@
+<?php
+require_once( 'BSMaintenance.php' );
+
+class BSPimpUnitTestResultsForJenkins extends BSMaintenance {
+	public function __construct() {
+		$this->addOption('source', 'The path to the source file', true, true);
+		$this->addOption('target', 'The path to the source file', true, true);
+		$this->addOption('prefix', 'A prefix for the whole test run', true, true);
+
+		parent::__construct();
+	}
+
+	public function execute() {
+		$originalReportFile = $this->getOption( 'source' );
+		if ( !file_exists( $originalReportFile ) ) {
+			die( "Cannot read input file" );
+		}
+
+		$prefix = $this->getOption( 'prefix' );
+
+		$reportDom = new DOMDocument();
+		$reportDom->load( $originalReportFile );
+
+		$root = $reportDom->documentElement;
+		$xpath = new DOMXPath( $reportDom );
+
+		$testcases = $root->getElementsByTagName( 'testcase' );
+
+		foreach ( $testcases as $testcase ) {
+			// Get suite name
+			// suitename can be like
+			// * BSApiTitleQueryStoreTest without DataProvider
+			// * BSApiTitleQueryStoreTest::testSingleFilter with DataProvider
+			$suitename = explode( "::", $testcase->parentNode->getAttribute( 'name' ) );
+			$suitename = $suitename[0];
+
+			// Get file name
+			// filename is absolute path or nothing if test has DataProvider. In this
+			// case, we need to go up one level further
+			$parentSuiteNode = $testcase->parentNode;
+			if ( !$parentSuiteNode->hasAttribute( 'file' ) ) {
+				$parentSuiteNode = $parentSuiteNode->parentNode;
+			}
+			$filename = preg_replace( "/^.*?extensions.(.*).tests.*$/", "$1", $parentSuiteNode->getAttribute( 'file' ) );
+			$filename = str_replace( ["/", "\\"], "::", $filename );
+
+			// Compile new class name for jenkins
+			$jenkinsClassName = $prefix . "." . $filename . "." . $suitename;
+
+			$testcase->setAttribute( "classname", $jenkinsClassName );
+		}
+
+		$reportDom->save( $this->getOption( 'target' ) );
+	}
+}
+
+$maintClass = 'BSPimpUnitTestResultsForJenkins';
+if (defined('RUN_MAINTENANCE_IF_MAIN')) {
+	require_once( RUN_MAINTENANCE_IF_MAIN );
+} else {
+	require_once( DO_MAINTENANCE ); # Make this work on versions before 1.17
+}
