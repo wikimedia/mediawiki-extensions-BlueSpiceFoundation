8d17487dda23f288578e2939e696a572da7c494a
Dejan Savuljesku <d.savuljesku@gmail.com>
2017-06-09 14:29:59 +0200

    API unittest: BSApiInterwikiStore
    Change-Id: I359654e2767f528c1267f6035864db4d8b6082da
    ERM: #6557

---
+++ tests/api/BSApiInterwikiStoreTest.php
@@ -0,0 +1,99 @@
+<?php
+
+/**
+ * @group medium
+ * @group api
+ * @group BlueSpice
+ * @group BlueSpiceFoundation
+ */
+class BSApiInterwikiStoreTest extends BSApiExtJSStoreTestBase {
+	protected $iFixtureTotal = 3;
+
+	protected function getStoreSchema() {
+		return [
+			'iw_prefix' => [
+				'type' => 'string'
+			],
+			'iw_url' => [
+				'type' => 'string'
+			],
+			'iw_api' => [
+				'type' => 'string'
+			],
+			'iw_wikiid' => [
+				'type' => 'string'
+			],
+			'iw_local' => [
+				'type' => 'string'
+			],
+			'iw_trans' => [
+				'type' => 'string'
+			]
+		];
+	}
+
+	protected function createStoreFixtureData() {
+		$oDbw = wfGetDB( DB_MASTER );
+		$oDbw->insert( 'interwiki', array(
+			'iw_prefix' => "Dummy",
+			'iw_url' => "http://wiki.dummy.org/$1",
+			'iw_local' => "1"
+		) );
+
+		$oDbw->insert( 'interwiki', array(
+			'iw_prefix' => "Demo",
+			'iw_url' => "http://wiki.demo.org/$1",
+			'iw_api' => "http://wiki.demo.org/api.php"
+		) );
+
+		$oDbw->insert( 'interwiki', array(
+			'iw_prefix' => "Sample",
+			'iw_url' => "http://wiki.sample.org/$1",
+			'iw_trans' => "1",
+			'iw_local' => "1"
+		) );
+		return 3;
+	}
+
+	protected function getModuleName() {
+		return 'bs-interwiki-store';
+	}
+
+	public function provideSingleFilterData() {
+		return [
+			'Filter by iw_url' => [ 'string', 'ct', 'iw_url', 'wiki', 3 ],
+			'Filter by iw_prefix' => [ 'string', 'eq', 'iw_prefix', 'Sample', 1 ]
+		];
+	}
+
+	public function provideMultipleFilterData() {
+		return [
+			'Filter by iw_local and iw_prefix' => [
+				[
+					[
+						'type' => 'string',
+						'comparison' => 'eq',
+						'field' => 'iw_local',
+						'value' => '1'
+					],
+					[
+						'type' => 'string',
+						'comparison' => 'eq',
+						'field' => 'iw_prefix',
+						'value' => 'Dummy'
+					]
+				],
+				1
+			]
+		];
+	}
+
+	public function provideKeyItemData() {
+		return array(
+			[ 'iw_url', 'http://wiki.demo.org/$1' ],
+			[ 'iw_api', 'http://wiki.demo.org/api.php' ],
+			[ 'iw_prefix', 'Demo' ]
+		);
+	}
+}
+
