473b91d262cc11ff7c30b2215496a3174975b586
Dejan Savuljesku <d.savuljesku@gmail.com>
2017-06-09 09:03:21 +0200

    API unittest: BSApiAdminUserStore and BSApiUserStore
    Patch Set 2: skipping total count test and adding key item test
    
    Patch Set 5: using brand new user fixture
    
    ERM: #6551, #6552
    
    Change-Id: I540d7042bcd4cf5191890ca2d20824fd2cdbfe5f

---
+++ tests/api/BSApiUserStoreTest.php
@@ -0,0 +1,90 @@
+<?php
+
+/**
+ * @group medium
+ * @group api
+ * @group Database
+ * @group BlueSpice
+ * @group BlueSpiceFoundation
+ */
+class BSApiUserStoreTest extends BSApiExtJSStoreTestBase {
+	protected function getStoreSchema() {
+		return [
+			'user_id' => [
+				'type' => 'integer'
+			],
+			'user_name' => [
+				'type' => 'string'
+			],
+			'user_real_name' => [
+				'type' => 'string'
+			],
+			'user_registration' => [
+				'type' => 'string'
+			],
+			'user_editcount' => [
+				'type' => 'integer'
+			],
+			'groups' => [
+				'type' => 'array'
+			],
+			'enabled' => [
+				'type' => 'boolean'
+			],
+			'page_link' => [
+				'type' => 'string'
+			],
+			'page_prefixed_text' => [
+				'type' => 'string'
+			]
+		];
+	}
+
+	protected function createStoreFixtureData() {
+		self::$userFixtures = new BSUserFixtures( $this );
+	}
+
+	protected function getModuleName() {
+		return 'bs-user-store';
+	}
+
+	public function provideSingleFilterData() {
+		return [
+			'Filter by user_name' => [ 'string', 'ct', 'user_name', 'John', 1 ]
+		];
+	}
+
+	public function provideMultipleFilterData() {
+		return [
+			'Filter by display_name and user_name' => [
+				[
+					[
+						'type' => 'string',
+						'comparison' => 'eq',
+						'field' => 'display_name',
+						'value' => 'John L.'
+					],
+					[
+						'type' => 'string',
+						'comparison' => 'ct',
+						'field' => 'user_name',
+						'value' => 'John'
+					]
+				],
+				1
+			]
+		];
+	}
+
+	public function provideKeyItemData() {
+		return[
+			'Test user John: name' => [ "user_name", "John" ],
+			'Test user Paul: name' => [ "user_name", "Paul" ],
+		];
+	}
+
+	protected function skipAssertTotal() {
+		return true;
+	}
+
+}
