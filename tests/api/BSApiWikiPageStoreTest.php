554144847b524a9a0558d1122e9e5da80d4865a5
Dejan Savuljesku <d.savuljesku@gmail.com>
2017-06-13 11:49:13 +0200

    API unittest: BSApiWikiPageStore
    Change-Id: If774f57c39184bec254ea877c43189d8d2c9060d
    ERM: #6591

---
+++ tests/api/BSApiWikiPageStoreTest.php
@@ -0,0 +1,79 @@
+<?php
+
+/**
+ * @group medium
+ * @group api
+ * @group BlueSpice
+ * @group BlueSpiceFoundation
+ */
+class BSApiWikiPageStoreTest extends BSApiExtJSStoreTestBase {
+
+	protected function skipAssertTotal() {
+		return true;
+	}
+
+	protected function getStoreSchema() {
+		return [
+			'page_id' => [
+				'type' => 'integer'
+			],
+			'page_namespace' => [
+				'type' => 'integer'
+			],
+			'page_title' => [
+				'type' => 'string'
+			]
+		];
+	}
+
+	protected function createStoreFixtureData() {
+		$oPageFixtures = new BSPageFixturesProvider();
+		$aFixtures = $oPageFixtures->getFixtureData();
+		foreach( $aFixtures as $aFixture ) {
+			$this->insertPage( $aFixture[0], $aFixture[1] );
+		}
+
+		return 0;
+	}
+
+	protected function getModuleName() {
+		return 'bs-wikipage-store';
+	}
+
+	public function provideSingleFilterData() {
+		return [
+			'Filter by page_title' => [ 'string', 'ct', 'page_title', 'Test', 2 ]
+		];
+	}
+
+	public function provideMultipleFilterData() {
+		return [
+			'Filter by page_namespace and page_title' => [
+				[
+					[
+						'type' => 'numeric',
+						'comparison' => 'eq',
+						'field' => 'page_namespace',
+						'value' => 12
+					],
+					[
+						'type' => 'string',
+						'comparison' => 'eq',
+						'field' => 'page_title',
+						'value' => 'Test'
+					]
+				],
+				1
+			]
+		];
+	}
+
+	public function provideKeyItemData() {
+		return array(
+			[ 'page_title', 'Зарегистрируйтесь_сейчас' ],
+			[ 'page_title', 'テスト' ],
+			[ 'page_namespace', 10 ]
+		);
+	}
+}
+
