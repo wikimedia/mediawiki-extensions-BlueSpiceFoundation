5f2d26f3e3ebf7f129b21d798e0fcae4bba7f90a
pwirth <wirth@hallowelt.biz>
2017-09-11 11:01:18 +0200

    BSFoundation: Added more hook base classes
    * GetPreferences
    * SkinTemplateOutputPageBeforeExec
    
    Change-Id: Ia7d142fd81b506a7894f4b63cb2c730ea200bb3c

---
+++ src/Hook/GetPreferences.php
@@ -0,0 +1,75 @@
+<?php
+/**
+ * Hook handler base class for MediaWiki hook GetPreferences
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ * This file is part of BlueSpice MediaWiki
+ * For further information visit http://bluespice.com
+ *
+ * @author     Patric Wirth <wirth@hallowelt.com>
+ * @package    BlueSpiceFoundation
+ * @copyright  Copyright (C) 2017 Hallo Welt! GmbH, All rights reserved.
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU Public License v2 or later
+ * @filesource
+ */
+namespace BlueSpice\Hook;
+use BlueSpice\Hook;
+
+abstract class GetPreferences extends Hook {
+
+	/**
+	 *
+	 * @var \User
+	 */
+	protected $user = null;
+
+	/**
+	 *
+	 * @var array
+	 */
+	protected $preferences = null;
+
+	/**
+	 *
+	 * @param \User $user
+	 * @param array $preferences
+	 * @return boolean
+	 */
+	public static function callback( $user, &$preferences ) {
+		$className = static::class;
+		$hookHandler = new $className(
+			null,
+			null,
+			$user,
+			$preferences
+		);
+		return $hookHandler->process();
+	}
+
+	/**
+	 *
+	 * @param \IContextSource $context
+	 * @param \Config $config
+	 * @param \User $user
+	 * @param array $preferences
+	 */
+	public function __construct( $context, $config, &$user, &$preferences ) {
+		parent::__construct( $context, $config );
+
+		$this->user = $user;
+		$this->preferences = &$preferences;
+	}
+}
