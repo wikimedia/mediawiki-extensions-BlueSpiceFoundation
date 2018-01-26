b6ba13bc721ae35638dcb80ebbc8e077c72e6881
dvogel <daniel.vogel@hallowelt.com>
2018-01-26 13:46:27 +0100

    SpecialPage WikiAdmin added to avoid issues with other extensions
    after removing WikiAdmin in BlueSpice 3
    
    Change-Id: Iae3d1ea5a907b9fe6f811ce82eeb6eb0a7f970ef

---
+++ includes/specials/SpecialWikiAdmin.class.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * Special page for WikiAdmin of BlueSpice (MediaWiki)
+ *
+ * Part of BlueSpice MediaWiki
+ *
+ * @author     Daniel Vogel
+ * @package    BlueSpiceFoundation
+ * @subpackage WikiAdmin
+ * @copyright  Copyright (C) 2016 Hallo Welt! GmbH, All rights reserved.
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU Public License v3
+ * @filesource
+ */
+class SpecialWikiAdmin extends BsSpecialPage {
+
+	public function __construct() {
+		parent::__construct( 'WikiAdmin', '', false );
+	}
+
+	public function execute( $par ) {
+		parent::execute( $par );
+		$oOutputPage = $this->getOutput();
+		$oOutputPage->setPageTitle( "WikiAdmin" );
+		$oOutputPage->addHTML( '<div>WikiAdmin has been removed in BlueSpice 3</div>' );
+	}
+
+	protected function getGroupName() {
+		return 'bluespice';
+	}
+
+}
---
+++ languages/BlueSpice.WikiAdmin.alias.php
@@ -0,0 +1,12 @@
+<?php
+$specialPageAliases = array();
+
+/** English */
+$specialPageAliases['en'] = array(
+	'WikiAdmin' => array( 'WikiAdmin' ),
+);
+
+/** German (Deutsch) */
+$specialPageAliases['de'] = array(
+	'WikiAdmin' => array( 'WikiAdmin' ),
+);
