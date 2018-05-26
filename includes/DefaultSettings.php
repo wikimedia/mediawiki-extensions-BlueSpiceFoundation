8f8c075ddae298fc56c54c0e55f5e8bebbd3f083
rvogel <vogel@hallowelt.biz>
2018-05-26 21:48:49 +0200

    Initialize `$bsgPermissionConfig`
    This variable was not initialized but used in `ServiceWiring.php`,
    thus creating a NOTICE.
    
    Change-Id: I64c6db1309d10a55ba26d47e7b5f862ba34b500a

--- includes/DefaultSettings.php
+++ includes/DefaultSettings.php
@@ -60,6 +60,8 @@ $GLOBALS[ 'bsgGroupRoles' ] = [
 
 $GLOBALS[ 'bsgNamespaceRolesLockdown' ]  = [];
 
+$GLOBALS[ 'bsgPermissionConfig' ] = [];
+
 $GLOBALS[ 'bsgPermissionConfigDefault' ] = array(
 	'read' => array(
 		'type' => 'namespace',
