174de89b2bbbcf71930816be86d9f52c55eb0f1f
Peter Böhm <boehm@hallowelt.com>
2018-06-21 16:56:41 +0200

    New Hook in BlueSpiceFoundation: SkinBuildSidebar
    Change-Id: I2dfb205872841f3621314737cdf2c5c6b9490ba4

---
+++ src/Hook/SkinBuildSidebar.php
@@ -0,0 +1,74 @@
+<?php
+/**
+ * Hook handler base class for MediaWiki hook SkinBuildSidebar
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ * This file is part of BlueSpice MediaWiki
+ * For further information visit http://bluespice.com
+ *
+ * @author     Peter Boehm <boehm@hallowelt.com>
+ * @package    BlueSpiceFoundation
+ * @copyright  Copyright (C) 2018 Hallo Welt! GmbH, All rights reserved.
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU Public License v3
+ * @filesource
+ */
+namespace BlueSpice\Hook;
+use BlueSpice\Hook;
+
+abstract class SkinBuildSidebar extends Hook {
+
+	/**
+	 *
+	 * @var \Skin
+	 */
+	protected $skin = null;
+
+	/**
+	 *
+	 * @var array
+	 */
+	protected $bar = [];
+
+	/**
+	 *
+	 * @param \Skin $skin
+	 * @param array $bar
+	 * @return boolean
+	 */
+	public static function callback( \Skin $skin, &$bar ) {
+		$className = static::class;
+		$hookHandler = new $className(
+			null,
+			null,
+			$skin,
+			$bar
+		);
+		return $hookHandler->process();
+	}
+
+	/**
+	 *
+	 * @param \IContextSource $context
+	 * @param \Config $config
+	 * @param \Skin $skin
+	 * @param array $bar
+	 */
+	public function __construct( $context, $config, $skin, &$bar ) {
+		parent::__construct( $context, $config );
+
+		$this->skin = $skin;
+		$this->bar = &$bar;
+	}
+}
