d3714f9c889404403020111b8f487c336062a211
pwirth <wirth@hallowelt.biz>
2018-06-28 10:45:54 +0200

    Filter: Fixed compatibillity when constructed from store with ExtJS6
    Change-Id: I85afdd2ea5a1d06b6dfcc0c1a1acc48240332175

--- src/Data/Filter.php
+++ src/Data/Filter.php
@@ -6,11 +6,37 @@ abstract class Filter {
 	const COMPARISON_EQUALS = 'eq';
 	const COMPARISON_NOT_EQUALS = 'neq';
 
-	const KEY_TYPE = 'type';
-	const KEY_COMPARISON = 'comparison';
-	const KEY_OPERATOR = 'operator';
+	/**
+	 * Prior in extjs 4. Default in all existing filters in BlueSpice
+	 */
+	const COMPARISON_CONTAINS = 'ct'; //
+
+	/**
+	 * Since extjs 6. Will be transformed into 'ct'
+	 */
+	const COMPARISON_LIKE = 'like'; //
+
+	/**
+	 * Prior in extjs 4. Default in all existing filters in BlueSpice
+	 */
 	const KEY_FIELD = 'field';
+
+	/**
+	 * Since extjs 6. Will be transformed into 'field'
+	 */
 	const KEY_PROPERTY = 'property';
+
+	/**
+	 * Prior in extjs 4. Default in all existing filters in BlueSpice
+	 */
+	const KEY_COMPARISON = 'comparison';
+
+	/**
+	 * Since extjs 6. Will be transformed into 'comparison'
+	 */
+	const KEY_OPERATOR = 'operator';
+
+	const KEY_TYPE = 'type';
 	const KEY_VALUE = 'value';
 
 	/**
@@ -46,9 +72,19 @@ abstract class Filter {
 			? $params[static::KEY_PROPERTY]
 			: $params[static::KEY_FIELD];
 		$this->value = $params[static::KEY_VALUE];
+		if( isset( $params[ static::KEY_OPERATOR] ) ) {
+			//compatibility. The comparison parameter changed into operator in
+			//ExtJs 6
+			$params[static::KEY_COMPARISON] = $params[ static::KEY_OPERATOR];
+		}
 		if( !isset($params[static::KEY_COMPARISON] ) ) {
 			$params[static::KEY_COMPARISON] = static::COMPARISON_EQUALS;
 		}
+		if( $params[static::KEY_COMPARISON] === static::COMPARISON_LIKE ) {
+			//compatibility. The comparison 'ct' changed into like in ExtJs 6
+			$params[static::KEY_COMPARISON] = static::COMPARISON_CONTAINS;
+		}
+
 		$this->comparison = $params[static::KEY_COMPARISON];
 	}
 
