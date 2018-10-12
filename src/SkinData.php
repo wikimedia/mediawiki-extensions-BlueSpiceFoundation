09731777649c6426afba0cc7c29068672773209e
rvogel <vogel@hallowelt.biz>
2018-10-12 12:25:09 +0200

    Add new SkinData fieldnames
    Change-Id: I48aa8d19e9f55c5f73839ac6794e5032ed3e9530

--- src/SkinData.php
+++ src/SkinData.php
@@ -29,5 +29,10 @@ class SkinData {
 	const AFTER_CONTENT = 'bs_after_content';
 
 	const EXPORT_MENU = 'bs_export_menu';
+	const EDIT_MENU = 'bs_edit_menu';
+	const VIEW_MENU = 'bs_view_menu';
+
+	const EDIT_MENU_BLACKLIST = 'bs_edit_menu_blacklist';
+	const VIEW_MENU_BLACKLIST = 'bs_view_menu_blacklist';
 	const TOOLBOX_BLACKLIST = 'bs_toolbox_blacklist';
 }
