8283cf838ebc9a13a92afd84f13f2212c53d3bbd
pwirth <wirth@hallowelt.biz>
2018-11-29 12:16:18 +0100

    Remove usage of BsLinkProvider and mark it as deprecated
    Also remove some old, not implemented methods
    
    => Needs cherry-pick to REL1_31
    
    Change-Id: Icc8cf2e3396a9f108326369c5ab8ffc3c5e33b54

--- includes/utility/LinkProvider.class.php
+++ includes/utility/LinkProvider.class.php
@@ -1,14 +1,20 @@
 <?php
 // Last review MRG (21.09.10 13:08)
 /**
+ * DEPRECATED!
  * This class provides functions to generate multiple types of links (WikiText or HTML anchors) from various sources.
  * Hint: https://www.mediawiki.org/wiki/Help:Links
+ * @deprecated since version 3.0.1 - Use \BlueSpice\Services::getInstance()
+ * ->getBSUtilityFactory()->getWikiTextLinksHelper and LinkRenderer service
+ * instead
  * @package BlueSpice_AdapterMW
  * @subpackage Utility
  */
 class BsLinkProvider {
 	/**
+	 * DEPRECATED!
 	 * Creates an HTML anchor tag (<a>) for the UserPage of the given User object. Allows additional configuration of this tag.
+	 * @deprecated since version 3.0.1 - use LinkRenderer service instead
 	 * @param User $oUser The MediaWiki User object a anchor tag should be created for.
 	 * @param String $sLabel If the link should have a special text instead of the users name, this parameter can contain it. By default the users real name (if set) or canonical username are used. I.e. $sLabel = 'the White Wizard'
 	 * @param String $sTitle This value will be used for the anchor tags "title" attribute. If nothing is provided the $sLable text will be used. I.e. $sTitle = 'Gandalf the Grey'
@@ -20,6 +26,7 @@ class BsLinkProvider {
 	 * @return String Something like '<a href="[...]index.php?title=Gandalf$action=edit" title="Gandalf" [...]>the White Wizard</a>'
 	 */
 	public static function makeAnchorTagForUser( User $oUser, $sLabel = '', $sTitle = '', $bRelative = true, $aAdditionalQueryString = array(), $aClasses = array(), $aStyles = array(), $aAdditionalAttributes = array() ) {
+		wfDebugLog( 'bluespice-deprecations', __METHOD__, 'private' );
 		$sUserDisplayName = \BlueSpice\Services::getInstance()
 			->getBSUtilityFactory()->getUserHelper( $oUser )->getDisplayName();
 
@@ -39,46 +46,9 @@ class BsLinkProvider {
 	}
 
 	/**
-	 * Creates a WikiText link to the provided users UserPage.
-	 * @param User $oUser The MediaWiki User object a wikilink should be created for.
-	 * @param String $sLabel If the link should have a special text instead of the users name, this parameter can contain it. By default the users real name (if set) or canonical username are used. I.e. $sLabel = 'Elessars life'
-	 * @param String $sJumpMark You can add a jumpmark to the link to go to a specific section of the page. I.e. $sJumpMark = 'vita'
-	 * @param Boolean $bUseCanonicalAsLabel Sometimes it's preferred to use the canonical username as lable instead of the real name. Use this flag for it.
-	 * @return String Something like [[Aragorn[#vita]|Elessars life]]
-	 */
-	public static function makeWikiLinkForUser( User $oUser, $sLabel = '', $sJumpMark = '', $bUseCanonicalAsLabel = false ) {
-		$sWikiLink = '';
-
-		// TODO MRG (21.09.10 12:50): implement...
-		throw new BsException('Not yet implemented');
-
-		return $sWikiLink;
-	}
-
-	//TODO: Add "$aAction" and "$arAdditionalQueryStringParameters" to the parameter list
-	/**
-	 * Creates an HTML anchor tag (<a>) for the given Title object. Allows additional configuration of this tag.
-	 * @param Title $oTitle The MediaWiki Title object a anchor tag should be created for.
-	 * @param String $sLabel If the link should have a special text instead of the articles title, this parameter can contain it. I.e. $sLabel = 'Home of Bilbo Baggins'
-	 * @param String $sTitle This value will be used for the anchor tags "title" attribute. If nothing is provided the $sLable text will be used. I.e. $sTitle = 'Bagend'
-	 * @param Boolean $bRelative Switch to make urls relative or absolute.
-	 * @param Array $aAdditionalQueryString If - for some reason - additional querystring parameters are needed, those can be provided as an associative array. I.e. $arAdditionalQueryString = array( 'action' => 'edit', 'knock' => 'three times') will result in something like 'href ="index.php?title=Bagend$action=edit&knock=tree%20times"' )
-	 * @param Array $aClasses Simple array of classes to be used within the "class" arrtibute of the anchor tag. I.e. $arClasses = array( 'green-underline', 'bold-text' ) will result in 'class="green-underline bold-text"'
-	 * @param Array $aStyles An associative array of css style statements. I.e. $arStyles = array( 'background-color' => '#00FF00', 'margin-left' => '10px' ) will result in 'style="background-color: #00FF00; margin-left: 10px;"'
-	 * @param Array $aAdditionalAttributes If special attributes are required, you can pass them in an associative array. Attention: attibutes that can be set by parameter (style, class, title, ...) are not allowed! I.e. $arAdditionalAttributes = array( 'target' => '_blank', 'name' => 'Bagend', 'title' => 'Not in the Shire' ) will result in <a href="[...]" title="Gandalf" [...] target="_blank" name="Bagend">[...]</a>
-	 * @return String Something like '<a href="[...]index.php?title=Bagend$action=edit" title="Home of Bilbo Baggins" [...]>Home of Bilbo Baggins</a>'
-	 */
-	public static function makeAnchorTagForTitle( Title $oTitle, $sLabel = '', $sTitle = '', $bRelative = true, $aAdditionalQueryString = array(), $aClasses = array(), $aStyles = array(), $aAdditionalAttributes = array() ) {
-		$sAnchorTag = '';
-
-		// TODO MRG (21.09.10 12:50): implement...
-		throw new BsException('Not yet implemented');
-
-		return $sAnchorTag;
-	}
-
-	/**
 	 * Creates a WikiText link to the provided Title. This is a wrapper function for makeWikiLinkForTitleString.
+	 * @deprecated since version 3.0.1 - Use \BlueSpice\Services::getInstance()
+	 * ->getBSUtilityFactory()->getWikiTextLinksHelper instead
 	 * @param Title $oTitle The MediaWiki Title object a wikilink should be created for.
 	 * @param String $sLabel If the link should have a alternative name instead of the title name, this parameter can contain it. I.e. $sLabel = 'Giant spider in Mordor'
 	 * @param String $sJumpMark You can add a jumpmark to the link to go to a specific section of the page. I.e. $sJumpMark = 'vita'
@@ -86,17 +56,20 @@ class BsLinkProvider {
 	 */
 	public static function makeWikiLinkForTitle( Title $oTitle, $sLabel = '', $sJumpMark = '' )
 	{
-		$sTitle = $oTitle->getText();
-		$sNamespaceText = $oTitle->getNsText();
-
-		$sWikiLink = BsLinkProvider::makeWikiLinkForTitleString($sTitle, $sNamespaceText, $sLabel, $sJumpMark);
-
-		return $sWikiLink;
+		wfDebugLog( 'bluespice-deprecations', __METHOD__, 'private' );
+		$util = \BlueSpice\Services::getInstance()->getBSUtilityFactory();
+		$linkHelper = $util->getWikiTextLinksHelper( '' )
+			->getInternalLinksHelper()->addTargets( [
+			$sLabel => $oTitle
+		] );
+		return $linkHelper->getWikitext();
 	}
 
 
 	/**
 	 * Creates a WikiText link to the provided Title.
+	 * @deprecated since version 3.0.1 - Use \BlueSpice\Services::getInstance()
+	 * ->getBSUtilityFactory()->getWikiTextLinksHelper instead
 	 * @param Title $sTitle The title string a wikilink should be created for.
 	 * @param String $sNamespace The namespace text string for the wikilink title.
 	 * @param String $sLabel If the link should have a alternative name instead of the title name, this parameter can contain it. I.e. $sLabel = 'Giant spider in Mordor'
@@ -105,54 +78,44 @@ class BsLinkProvider {
 	 */
 	public static function makeWikiLinkForTitleString( $sTitle, $sNamespaceText = '', $sLabel = '', $sJumpMark = '' )
 	{
-		$sWikiLink = "[[";
-	// TODO MRG (21.09.10 12:54): Es gibt noch den Fall, dass ich explizit in den Main-Space verlinken möchte,
-	// so: [[:Test]]. Wichtiger noch: auf eine Kategorie-Seite verlinken (nicht eine Kategorie zuordnen) geht so:
-	// [[:Kategorie:Test]]. Das sollte hier berücksichtigt werden könnnen.
-		if ( trim($sNamespaceText) != '' ) {
-			$sWikiLink .= "$sNamespaceText:";
+		wfDebugLog( 'bluespice-deprecations', __METHOD__, 'private' );
+		if( !empty( $sNamespaceText ) ) {
+			$sNamespaceText .= ':';
 		}
-		$sWikiLink .= $sTitle;
-		if ( trim($sJumpMark) != '' ) {
-			$sWikiLink .= "#$sJumpMark";
-		}
-		if ( trim($sLabel) != '' ) {
-			$sWikiLink .= "|$sLabel";
-		}
-		$sWikiLink .= "]]";
-
-		return $sWikiLink;
+		return static::makeWikiLinkForTitle(
+			\Title::newFromText( $sNamespaceText . $sTitle ),
+			$sLabel,
+			$sJumpMark
+		);
 	}
 
 
 	/**
+	 * DEPRECATED!
 	 * Creates a WikiText Link in the form of &quot;[[NSP:Title|Description]]&quot;
 	 * and escapes it if the Title is from NS_CATEGORY or NS_FILE to aviod
 	 * parsing problems.
+	 * @deprecated since version 3.0.1 - Use \BlueSpice\Services::getInstance()
+	 * ->getBSUtilityFactory()->getWikiTextLinksHelper instead
 	 * @param Title $oTitle The MediaWiki Title object to link to.
 	 * @param string $sDescription The description part of the link.
 	 * @return string The WikiText link
 	 */
 	public static function makeEscapedWikiLinkForTitle( $oTitle, $sDescription = '' ) {
-		if ( !( $oTitle instanceof Title ) ) return '';
-
-		$sWikiLink = '[[';
-
-		if ( in_array( $oTitle->getNamespace(), array( NS_FILE, NS_CATEGORY ) ) ) {
-			$sWikiLink .= ':'; //Prevent file links from being rendered as <img .../> tags and category links as... nothing.
-		}
-
-		$sWikiLink .= $oTitle->getPrefixedText();
-
-		if( !empty( $sDescription ) ) {
-			$sWikiLink .= '|'.$sDescription;
-		}
-
-		$sWikiLink .= ']]';
-		return $sWikiLink;
+		wfDebugLog( 'bluespice-deprecations', __METHOD__, 'private' );
+		return static::makeWikiLinkForTitle( $oTitle, $sDescription );
 	}
 
+	/**
+	 * DEPRECATED!
+	 * @deprecated since version 3.0.1 - not in use anymore
+	 * @param array $aClasses
+	 * @param array $aStyles
+	 * @param array $aAdditionalAttributes
+	 * @return string
+	 */
 	private static function makeAttributesFromArrays( $aClasses = array(), $aStyles = array(), $aAdditionalAttributes = array() ) {
+		wfDebugLog( 'bluespice-deprecations', __METHOD__, 'private' );
 		// TODO MRG (21.09.10 12:53): ich würde generell das Vorgehen über Option-Arrays bevorzugen. Warum sollten Klassen
 		// und Styles eine Sonderstellung haben?
 		$sAttributes = '';
@@ -181,7 +144,9 @@ class BsLinkProvider {
 	}
 
 	/**
+	 * DEPRECATED!
 	 * Provides a linker link
+	 * @deprecated since version 3.0.1 - use LinkRenderer service instead
 	 * @param Title $oTitle
 	 * @param String|null $sHtml
 	 * @param Array $aCustomAttribs
@@ -190,6 +155,7 @@ class BsLinkProvider {
 	 * @return String Link
 	 */
 	public static function makeLink( $oTitle, $sHtml = null, $aCustomAttribs = array(), $aQuery = array(), $aOptions = array() ) {
+		wfDebugLog( 'bluespice-deprecations', __METHOD__, 'private' );
 		return Linker::link( $oTitle, $sHtml, $aCustomAttribs, $aQuery, $aOptions );
 	}
 
