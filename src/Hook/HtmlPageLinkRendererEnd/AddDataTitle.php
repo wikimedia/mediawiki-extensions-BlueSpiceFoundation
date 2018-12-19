fffc6843ec9cbe4fbebc7e1c747328497cc54a92
Dejan Savuljesku <d.savuljesku@gmail.com>
2018-12-19 10:21:07 +0100

    Put prefixed page title in data-bs-title
    bs-title data attribute is used in many places to determine which page
    the link is referencing. This has to be prefixed, otherwise all pages
    will point to NS_MAIN.
    
    Problems encountered with ContextMenu, where file links offered no
    normal file options.
    
    Change-Id: I5016ed27d12cd9253fd951611eaed3bf3fb710ac

--- src/Hook/HtmlPageLinkRendererEnd/AddDataTitle.php
+++ src/Hook/HtmlPageLinkRendererEnd/AddDataTitle.php
@@ -25,7 +25,8 @@ class AddDataTitle extends \BlueSpice\Hook\HtmlPageLinkRendererEnd {
 	protected function doProcess() {
 		//We add the original title to a link. This may be the same content as
 		//"title" attribute, but it doesn't have to. I.e. in red links
-		$this->attribs['data-bs-title'] = $this->target->getDBkey();
+		$title = \Title::newFromLinkTarget( $this->target );
+		$this->attribs['data-bs-title'] = $title->getPrefixedDBkey();
 
 		if( $this->target->getNamespace() === NS_FILE ) {
 			$this->attribs['data-bs-filename'] = $this->target->getText();
