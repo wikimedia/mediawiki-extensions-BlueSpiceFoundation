f97b26ab3045cf2734c8a6a01743aa549be3f031
zoranzoki21 <zorandori4444@gmail.com>
2018-12-31 04:38:18 +0100

    Add Bosnian alias and cleanup *.alias.php files
    Change-Id: Ia57915bc8dbfebcea14fe377689f7691c9fc091f

--- languages/BlueSpice.Credits.alias.php
+++ languages/BlueSpice.Credits.alias.php
@@ -1,108 +1,113 @@
 <?php
 
-$specialPageAliases = array();
+$specialPageAliases = [];
 
 /** English (English) */
-$specialPageAliases['en'] = array(
-	'SpecialCredits' => array( 'Credits' ),
-);
+$specialPageAliases['en'] = [
+	'SpecialCredits' => [ 'Credits' ],
+];
 
 /** Arabic (العربية) */
-$specialPageAliases['ar'] = array(
-	'SpecialCredits' => array( 'ائتمانات' ),
-);
+$specialPageAliases['ar'] = [
+	'SpecialCredits' => [ 'ائتمانات' ],
+];
 
 /** Egyptian Arabic (مصرى) */
-$specialPageAliases['arz'] = array(
-	'SpecialCredits' => array( 'ائتمانات' ),
-);
+$specialPageAliases['arz'] = [
+	'SpecialCredits' => [ 'ائتمانات' ],
+];
 
 /** Western Balochi (بلوچی رخشانی) */
-$specialPageAliases['bgn'] = array(
-	'SpecialCredits' => array( 'Credits' ),
-);
+$specialPageAliases['bgn'] = [
+	'SpecialCredits' => [ 'Credits' ],
+];
+
+/** Bosnian (bosnian) */
+$specialPageAliases['bs'] = [
+	'SpecialCredits' => [ 'Zasluge' ],
+];
 
 /** Czech (čeština) */
-$specialPageAliases['cs'] = array(
-	'SpecialCredits' => array( 'Zásluhy', 'Zasluhy' ),
-);
+$specialPageAliases['cs'] = [
+	'SpecialCredits' => [ 'Zásluhy', 'Zasluhy' ],
+];
 
 /** German (Deutsch) */
-$specialPageAliases['de'] = array(
-	'SpecialCredits' => array( 'Danksagungen' ),
-);
+$specialPageAliases['de'] = [
+	'SpecialCredits' => [ 'Danksagungen' ],
+];
 
 /** Zazaki (Zazaki) */
-$specialPageAliases['diq'] = array(
-	'SpecialCredits' => array( 'Krediyi' ),
-);
+$specialPageAliases['diq'] = [
+	'SpecialCredits' => [ 'Krediyi' ],
+];
 
 /** Upper Sorbian (hornjoserbsce) */
-$specialPageAliases['hsb'] = array(
-	'SpecialCredits' => array( 'Dźakprajenja' ),
-);
+$specialPageAliases['hsb'] = [
+	'SpecialCredits' => [ 'Dźakprajenja' ],
+];
 
 /** Italian (italiano) */
-$specialPageAliases['it'] = array(
-	'SpecialCredits' => array( 'Crediti' ),
-);
+$specialPageAliases['it'] = [
+	'SpecialCredits' => [ 'Crediti' ],
+];
 
 /** Korean (한국어) */
-$specialPageAliases['ko'] = array(
-	'SpecialCredits' => array( '제작진' ),
-);
+$specialPageAliases['ko'] = [
+	'SpecialCredits' => [ '제작진' ],
+];
 
 /** Luxembourgish (Lëtzebuergesch) */
-$specialPageAliases['lb'] = array(
-	'SpecialCredits' => array( 'Merci_un' ),
-);
+$specialPageAliases['lb'] = [
+	'SpecialCredits' => [ 'Merci_un' ],
+];
 
 /** Northern Luri (لۊری شومالی) */
-$specialPageAliases['lrc'] = array(
-	'SpecialCredits' => array( 'ئتئڤاریا' ),
-);
+$specialPageAliases['lrc'] = [
+	'SpecialCredits' => [ 'ئتئڤاریا' ],
+];
 
 /** Macedonian (македонски) */
-$specialPageAliases['mk'] = array(
-	'SpecialCredits' => array( 'Заслуги' ),
-);
+$specialPageAliases['mk'] = [
+	'SpecialCredits' => [ 'Заслуги' ],
+];
 
 /** Malayalam (മലയാളം) */
-$specialPageAliases['ml'] = array(
-	'SpecialCredits' => array( 'കടപ്പാടുകൾ' ),
-);
+$specialPageAliases['ml'] = [
+	'SpecialCredits' => [ 'കടപ്പാടുകൾ' ],
+];
 
 /** Dutch (Nederlands) */
-$specialPageAliases['nl'] = array(
-	'SpecialCredits' => array( 'Naamsvermeldingen' ),
-);
+$specialPageAliases['nl'] = [
+	'SpecialCredits' => [ 'Naamsvermeldingen' ],
+];
 
 /** Brazilian Portuguese (português do Brasil) */
-$specialPageAliases['pt-br'] = array(
-	'SpecialCredits' => array( 'Créditos' ),
-);
+$specialPageAliases['pt-br'] = [
+	'SpecialCredits' => [ 'Créditos' ],
+];
 
 /** Serbian Cyrillic (српски (ћирилица)) */
-$specialPageAliases['sr-ec'] = array(
-	'SpecialCredits' => array( 'Заслуге' ),
-);
+$specialPageAliases['sr-ec'] = [
+	'SpecialCredits' => [ 'Заслуге' ],
+];
 
 /** Serbian Latin (srpski (latinica)) */
-$specialPageAliases['sr-el'] = array(
-	'SpecialCredits' => array( 'Zasluge' ),
-);
+$specialPageAliases['sr-el'] = [
+	'SpecialCredits' => [ 'Zasluge' ],
+];
 
 /** Vietnamese (Tiếng Việt) */
-$specialPageAliases['vi'] = array(
-	'SpecialCredits' => array( 'Ghi_công' ),
-);
+$specialPageAliases['vi'] = [
+	'SpecialCredits' => [ 'Ghi_công' ],
+];
 
 /** Simplified Chinese (中文（简体）‎) */
-$specialPageAliases['zh-hans'] = array(
-	'SpecialCredits' => array( '制作组' ),
-);
+$specialPageAliases['zh-hans'] = [
+	'SpecialCredits' => [ '制作组' ],
+];
 
 /** Traditional Chinese (中文（繁體）‎) */
-$specialPageAliases['zh-hant'] = array(
-	'SpecialCredits' => array( '製作群' ),
-);
+$specialPageAliases['zh-hant'] = [
+	'SpecialCredits' => [ '製作群' ],
+];
