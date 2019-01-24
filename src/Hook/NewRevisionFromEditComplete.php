e4a067c69a44a99358f1bae2b001706acd005664
Dejan Savuljesku <d.savuljesku@gmail.com>
2019-01-24 12:45:19 +0100

    Add base hook handler for NewRevisionFromEditComplete
    Change-Id: I70abc9701053239515f2687073ad8197d9bcdf88

---
+++ src/Hook/NewRevisionFromEditComplete.php
@@ -0,0 +1,89 @@
+<?php
+/**
+ * Hook handler base class for MediaWiki hook NewRevisionFromEditComplete
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ * This file is part of BlueSpice MediaWiki
+ * For further information visit http://bluespice.com
+ *
+ * @author     Dejan Savuljesku <savuljesku@hallowelt.com>
+ * @package    BlueSpiceFoundation
+ * @copyright  Copyright (C) 2017 Hallo Welt! GmbH, All rights reserved.
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU Public License v3
+ * @filesource
+ */
+namespace BlueSpice\Hook;
+use BlueSpice\Hook;
+
+abstract class NewRevisionFromEditComplete extends Hook {
+
+	/**
+	 * @var \WikiPage
+	 */
+	protected $wikiPage;
+
+	/**
+	 * @var \Revision
+	 */
+	protected $rev;
+
+	/**
+	 * @var integer
+	 */
+	protected $baseID;
+
+	/**
+	 * @var \User
+	 */
+	protected $user;
+
+	/**
+	 * @param \WikiPage $wikiPage
+	 * @param \Revision $rev
+	 * @param int $baseID
+	 * @param \User $user
+	 * @return bool
+	 */
+	public static function callback( $wikiPage, $rev, $baseID, $user ) {
+		$className = static::class;
+		$hookHandler = new $className(
+			null,
+			null,
+			$wikiPage,
+			$rev,
+			$baseID,
+			$user
+		);
+		return $hookHandler->process();
+	}
+
+	/**
+	 *
+	 * @param \IContextSource $context
+	 * @param \Config $config
+	 * @param \WikiPage $wikiPage
+	 * @param \Revision $rev
+	 * @param int $baseID
+	 * @param \User $user
+	 */
+	public function __construct( $context, $config, $wikiPage, $rev, $baseID, $user ) {
+		parent::__construct( $context, $config );
+
+		$this->wikiPage = $wikiPage;
+		$this->rev = $rev;
+		$this->baseID = $baseID;
+		$this->user = $user;
+	}
+}
