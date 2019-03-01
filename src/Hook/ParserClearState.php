67966233ffd4ece1192b95a26468364ed2f8bd16
pwirth <wirth@hallowelt.biz>
2019-03-01 13:46:57 +0100

    Add OutputPageParserOutput and ParserClearState hook handler base classes
    => neewd cherry-pick to REL1_31
    
    Change-Id: I8a7c1f02a84fe224457309c5de0522d3b56db360

---
+++ src/Hook/ParserClearState.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * Hook handler base class for MediaWiki hook ParserClearState
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ * This file is part of BlueSpice MediaWiki
+ * For further information visit http://bluespice.com
+ *
+ * @author     Patric Wirth <wirth@hallowelt.com>
+ * @package    BlueSpiceFoundation
+ * @copyright  Copyright (C) 2019 Hallo Welt! GmbH, All rights reserved.
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU Public License v3
+ * @filesource
+ */
+namespace BlueSpice\Hook;
+
+use Parser;
+use BlueSpice\Hook;
+
+abstract class ParserClearState extends Hook {
+
+	/**
+	 *
+	 * @var Parser
+	 */
+	protected $parser = null;
+
+	/**
+	 *
+	 * @param Parser $parser
+	 * @return boolean
+	 */
+	public static function callback( &$parser ) {
+		$className = static::class;
+		$hookHandler = new $className(
+			null,
+			null,
+			$parser
+		);
+		return $hookHandler->process();
+	}
+
+	/**
+	 *
+	 * @param \IContextSource $context
+	 * @param \Config $config
+	 * @param Parser $parser
+	 */
+	public function __construct( $context, $config, &$parser ) {
+		parent::__construct( $context, $config );
+
+		$this->parser =& $parser;
+	}
+}
