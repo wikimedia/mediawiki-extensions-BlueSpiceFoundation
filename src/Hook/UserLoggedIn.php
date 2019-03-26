89972911672f365ec418017f1cc51179fb10b4d4
pwirth <wirth@hallowelt.biz>
2019-03-26 11:03:38 +0100

    Add base class for hook UserLoggedIn
    => Needs cherry-pick to REL1_31
    
    Change-Id: Id4dc4761fb593d04ecc5ab20c8de1c918c86dd4d

---
+++ src/Hook/UserLoggedIn.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * Hook handler base class for MediaWiki hook UserLoggedIn
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ * This file is part of BlueSpice MediaWiki
+ * For further information visit http://bluespice.com
+ *
+ * @author     Patric Wirth <wirth@hallowelt.com>
+ * @package    BlueSpiceFoundation
+ * @copyright  Copyright (C) 2019 Hallo Welt! GmbH, All rights reserved.
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU Public License v3
+ * @filesource
+ */
+namespace BlueSpice\Hook;
+
+use User;
+use IContextSource;
+use Config;
+use BlueSpice\Hook;
+
+abstract class UserLoggedIn extends Hook {
+
+	/**
+	 *
+	 * @var User
+	 */
+	protected $user = null;
+
+	/**
+	 *
+	 * @param User $user
+	 * @return boolean
+	 */
+	public static function callback( $user ) {
+		$className = static::class;
+		$hookHandler = new $className(
+			null,
+			null,
+			$user
+		);
+		return $hookHandler->process();
+	}
+
+	/**
+	 *
+	 * @param IContextSource $context
+	 * @param Config $config
+	 * @param User $user
+	 */
+	public function __construct( $context, $config, $user ) {
+		parent::__construct( $context, $config );
+
+		$this->user = $user;
+	}
+}
