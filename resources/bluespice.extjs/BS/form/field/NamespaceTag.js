6bbb44239a01008dd993fe2dcdc231c9e8fb9793
rvogel <vogel@hallowelt.biz>
2019-04-05 16:06:12 +0200

    Add new input type `BS.form.field.NamespaceTag`
    Authored by Alexander Pinchuk and Dmitry Shevliakov
    
    NEEDS CHEERY-PICK TO `master`
    
    Change-Id: Ia60757a9e42d8f145106b0384854afbff751d7cd

---
+++ resources/bluespice.extjs/BS/form/field/NamespaceTag.js
@@ -0,0 +1,25 @@
+Ext.define( 'BS.form.field.NamespaceTag', {
+	extend: 'Ext.form.field.Tag',
+	displayField: 'namespace',
+	labelAlign: 'right',
+	valueField: 'id',
+	queryMode: 'local',
+	typeAhead: true,
+	triggerAction: 'all',
+	multiSelect: true,
+	collapseOnSelect: true,
+	fieldLabel: mw.message('bs-extjs-label-namespace').plain(),
+	emptyText: mw.message( "bs-extjs-combo-box-default-placeholder" ).plain(),
+	delimiter: ', ',
+	//Custom Settings
+	includeAll: false,
+	excludeIds: [],
+
+	initComponent: function() {
+		this.store = bs.extjs.newLocalNamespacesStore({
+			includeAll: this.includeAll,
+			excludeIds: this.excludeIds
+		});
+		this.callParent(arguments);
+	}
+});
