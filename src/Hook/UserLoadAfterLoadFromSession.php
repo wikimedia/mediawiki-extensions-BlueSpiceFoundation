f697b57cb4d9c6195a450219f3543f2045b694ae
pwirth <wirth@hallowelt.biz>
2019-05-22 15:35:33 +0200

    Add UserLoadAfterLoadFromSession hook hander base class
    => Needs cherry-pick to REL1_31_dev and REL1_31
    
    Change-Id: I7ba6aa447165967a38a8dba1ffae2c9d807e279f

---
+++ src/Hook/UserLoadAfterLoadFromSession.php
@@ -0,0 +1,72 @@
+<?php
+/**
+ * Hook handler base class for MediaWiki hook UserLoadAfterLoadFromSession
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ * This file is part of BlueSpice MediaWiki
+ * For further information visit http://bluespice.com
+ *
+ * @author     Patric Wirth <wirth@hallowelt.com>
+ * @package    BlueSpiceFoundation
+ * @copyright  Copyright (C) 2019 Hallo Welt! GmbH, All rights reserved.
+ * @license    http://www.gnu.org/copyleft/gpl.html GPL-3.0-only
+ * @filesource
+ */
+namespace BlueSpice\Hook;
+
+use IContextSource;
+use Config;
+use User;
+use BlueSpice\Hook;
+
+/**
+ * Called to authenticate users on external/environmental means; occurs after session
+ * is loaded
+ */
+abstract class UserLoadAfterLoadFromSession extends Hook {
+
+	/**
+	 *
+	 * @var User
+	 */
+	protected $user = null;
+
+	/**
+	 *
+	 * @param User $user
+	 * @return bool
+	 */
+	public static function callback( $user ) {
+		$className = static::class;
+		$hookHandler = new $className(
+			null,
+			null,
+			$user
+		);
+		return $hookHandler->process();
+	}
+
+	/**
+	 *
+	 * @param IContextSource $context
+	 * @param Config $config
+	 * @param User $user
+	 */
+	public function __construct( $context, $config, $user ) {
+		parent::__construct( $context, $config );
+
+		$this->user = $user;
+	}
+}
