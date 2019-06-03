e48ae08264750bfaf28034f8e3742a8063a97812
Dejan Savuljesku <d.savuljesku@gmail.com>
2019-06-03 16:01:50 +0200

    Remove reader role from anon users in default pm-settings.php
    Due to change gerrit:513999 it makes sense to make this change here as well
    
    Needs cherry-picking to REL1_31_dev and REL1_31
    
    Change-Id: Ibb7f8f16afc97076cadcfe2fb578727354123663
    ERM: #14063

--- config.template/pm-settings.php
+++ config.template/pm-settings.php
@@ -1,5 +1,5 @@
 <?php
-$GLOBALS['bsgGroupRoles']['*']['reader'] = true;
+$GLOBALS['bsgGroupRoles']['user']['reader'] = true;
 $GLOBALS['bsgGroupRoles']['user']['editor'] = true;
 $GLOBALS['bsgGroupRoles']['bureaucrat']['accountmanager'] = true;
 $GLOBALS['bsgGroupRoles']['bot']['bot'] = true;
