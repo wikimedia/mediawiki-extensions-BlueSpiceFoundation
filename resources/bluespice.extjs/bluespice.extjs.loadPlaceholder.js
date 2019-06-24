fd463e50c97a86631b33e80ad674d6019f0b73fd
Dejan Savuljesku <d.savuljesku@gmail.com>
2019-06-24 11:12:51 +0200

    Make loadPlaceholders more generic
    Even non-manager SPs can use loadPlaceholders, now they can extend ExtJSBase SP
    to receive this functionality
    
    [3.1]
    
    Change-Id: Ia53630926fd909fecbae12e5e02962dd9704cedf
    ERM: #14896

--- resources/bluespice.extjs/bluespice.extjs.loadPlaceholder.js
+++ resources/bluespice.extjs/bluespice.extjs.loadPlaceholder.js
@@ -1,21 +1,21 @@
 ( function ( mw, bs, $, d,undefined ) {
 	// DOM ready
 	$( function() {
-		var $manager, config, callback, observer, i;
+		var $cnt, config, callback, observer, i;
 
-		$manager = $( '.bs-manager-container' );
+		$cnt = $( '.extjs-load-placeholder-cnt' );
 
 		// Detect rendering of ExtJS component in the container
 		// this is done in effort to make loading indicator automatic
 		// without the need for each manager extension to call popPending()
-		if ( $manager.length === 0 ) {
+		if ( $cnt.length === 0 ) {
 			return;
 		}
-		if ( $manager.find( '.x-panel' ).length > 0 ) {
+		if ( $cnt.find( '.x-panel' ).length > 0 ) {
 			// ExtJS component already rendered, stop loading right away
 			// NOTE: This is a generic implementation, if extension is using some
 			// non-standard components, its responsible for popping the loading indicator
-			return $manager.find( '.placeholder-loader' ).remove();
+			return $cnt.find( '.placeholder-loader' ).remove();
 		}
 		// ExtJS component not rendered yet - watch for mutations
 		config = {
@@ -24,11 +24,11 @@
 		callback = function( mutationList, observer ) {
 			for ( i = 0; i < mutationList.length; i++ ) {
 				if ( mutationList[i].type === 'childList' ) {
-					$manager.find( '.placeholder-loader' ).remove();
+					$cnt.find( '.placeholder-loader' ).remove();
 				}
 			}
 		};
 		observer = new MutationObserver( callback );
-		observer.observe( $manager[0], config );
+		observer.observe( $cnt[0], config );
 	} );
 }( mediaWiki, blueSpice, jQuery, document ) );
--- src/Special/ManagerBase.php
+++ src/Special/ManagerBase.php
@@ -2,77 +2,15 @@
 
 namespace BlueSpice\Special;
 
-use BlueSpice\LoadPlaceholderRegistry;
-use BlueSpice\SpecialPage;
-use Html;
+abstract class ManagerBase extends ExtJSBase {
 
-abstract class ManagerBase extends SpecialPage {
-
-	public function execute( $par ) {
-		parent::execute( $par );
-
-		$this->addContainer();
-		$this->addJSVars();
-		$this->addModules();
-	}
-
-	protected function addContainer() {
-		$attributes = array_merge( [
-			'id' => $this->getId(),
-			'class' => 'bs-manager-container'
- 		], $this->getAttributes() );
-		$container = Html::openElement( 'div', $attributes );
-		$container .= $this->getLoadPlaceholder();
-		$container .= Html::closeElement( 'div' );
-
-		$this->getOutput()->addHTML( $container );
-	}
-
-	protected function addJSVars() {
-		$vars = $this->getJSVars();
-		if ( empty( $vars ) ) {
-			return;
-		}
-		$this->getOutput()->addJsConfigVars( $vars );
-	}
-
-	protected function addModules() {
-		$this->getOutput()->addModules( $this->getModules() );
-	}
-
-	/**
-	 * @return string ID of the HTML element being added
-	 */
-	abstract protected function getId();
-
-	/**
-	 * @return array
-	 */
-	abstract protected function getModules();
-
-	/**
-	 * Return array of attributes to be added
-	 * to the HTML element
-	 *
-	 * @return array [ $attr => $value ]
-	 */
-	protected function getAttributes() {
-		return [];
+	protected function getClasses() {
+		return parent::getClasses() + [
+				'bs-manager-container'
+		];
 	}
 
 	protected function getLoadPlaceholderTemplateName() {
 		return 'CRUDGrid';
-	}
-
-	/**
-	 * @return array [ $name => $value ]
-	 */
-	protected function getJSVars() {
-		return [];
-	}
-
-	private function getLoadPlaceholder() {
-		$registry = new LoadPlaceholderRegistry();
-		return $registry->getParsedTemplate( $this->getLoadPlaceholderTemplateName() );
 	}
 }
