2d3e1ab361469abb246e1be27531a0d2c77ebd20
rvogel <vogel@hallowelt.biz>
2019-08-07 10:36:43 +0200

    Move favicon setting to section `Skinning`
    Also remove BlueSpice favicon, as this is now part it the BlueSpiceCalumma skin
    
    ERM16046
    
    NEEDS CHERRY-PICK TO `master`!
    
    Change-Id: Ie55c5edf54c1e3ca81e039035fcd2cec193f859d

--- src/ConfigDefinition/Favicon.php
+++ src/ConfigDefinition/Favicon.php
@@ -4,10 +4,30 @@ namespace BlueSpice\ConfigDefinition;
 
 class Favicon extends StringSetting {
 
+	/**
+	 *
+	 * @return string[]
+	 */
+	public function getPaths() {
+		return [
+			static::MAIN_PATH_FEATURE . '/' . static::FEATURE_SKINNING . '/' . static::EXTENSION_FOUNDATION,
+			static::MAIN_PATH_EXTENSION . '/' . static::EXTENSION_FOUNDATION . '/' . static::FEATURE_SKINNING,
+			static::MAIN_PATH_PACKAGE . '/' . static::PACKAGE_FREE . '/' . static::EXTENSION_FOUNDATION,
+		];
+	}
+
+	/**
+	 *
+	 * @return string
+	 */
 	public function getLabelMessageKey() {
 		return 'bs-pref-faviconpath';
 	}
 
+	/**
+	 *
+	 * @return string
+	 */
 	public function getVariableName() {
 		return 'wg' . $this->getName();
 	}
