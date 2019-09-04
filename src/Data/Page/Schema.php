9217b3bf56d6186677f8abde0d61bc21fd338339
pwirth <wirth@hallowelt.biz>
2019-09-04 15:43:47 +0200

    Add basic page store classes
    [3.1]
    
    Change-Id: I80f613923844882a85adb52d82ffa6096ee24b6d

---
+++ src/Data/Page/Schema.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace BlueSpice\Data\Page;
+
+use BlueSpice\Data\FieldType;
+
+class Schema extends \BlueSpice\Data\Schema {
+	const TABLE_NAME = 'page';
+
+	public function __construct() {
+		parent::__construct( [
+			Record::ID => [
+				self::FILTERABLE => true,
+				self::SORTABLE => true,
+				self::TYPE => FieldType::INT
+			],
+			Record::NS => [
+				self::FILTERABLE => true,
+				self::SORTABLE => true,
+				self::TYPE => FieldType::INT
+			],
+			Record::TITLE => [
+				self::FILTERABLE => true,
+				self::SORTABLE => true,
+				self::TYPE => FieldType::STRING
+			],
+			Record::IS_REDIRECT => [
+				self::FILTERABLE => true,
+				self::SORTABLE => true,
+				self::TYPE => FieldType::BOOLEAN
+			],
+			Record::ID_NEW => [
+				self::FILTERABLE => true,
+				self::SORTABLE => true,
+				self::TYPE => FieldType::BOOLEAN
+			],
+			Record::TOUCHED => [
+				self::FILTERABLE => true,
+				self::SORTABLE => true,
+				self::TYPE => FieldType::DATE
+			],
+			Record::LATEST => [
+				self::FILTERABLE => true,
+				self::SORTABLE => true,
+				self::TYPE => FieldType::INT
+			],
+			Record::CONTENT_MODEL => [
+				self::FILTERABLE => true,
+				self::SORTABLE => true,
+				self::TYPE => FieldType::STRING
+			],
+		] );
+	}
+}
