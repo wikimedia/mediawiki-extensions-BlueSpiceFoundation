d679a25b42b5603c9668697bdf52e564163bafda
Robert Vogel <vogel@hallowelt.biz>
2019-09-16 10:59:49 +0000

    Revert "addCategory fix"
    This reverts commit 228e06a3e191077bf9471716b9e12440f8ccb0a8.
    
    Change-Id: I136ae765b7a3fcee135141cc73a195d55f241715

--- src/Task/WikiPage/AddCategories.php
+++ src/Task/WikiPage/AddCategories.php
@@ -3,37 +3,9 @@
 namespace BlueSpice\Task\WikiPage;
 
 use Title;
-use Status;
-use WikiPage;
-use Category;
-use WikiCategoryPage;
-use BlueSpice\ParamProcessor\ParamType;
-use BlueSpice\ParamProcessor\ParamDefinition;
 use BlueSpice\Utility\WikiTextLinksHelper\CategoryLinksHelper;
 
 class AddCategories extends SetCategories {
-
-	/**
-	 * @var WikiCategoryPage
-	 */
-	private $wikiCategoryPage = null;
-
-	const PARAM_PAGE_TITLE = 'page_title';
-
-	/**
-	 * @return Status
-	 */
-	protected function doExecute() {
-		return $this->saveWikiPage( '' );
-	}
-
-	/**
-	 *
-	 * @return string[]
-	 */
-	public function getTaskPermissions() {
-		return [ 'create' ];
-	}
 
 	/**
 	 *
@@ -44,6 +16,7 @@ class AddCategories extends SetCategories {
 	protected function getCategoriesToRemove( CategoryLinksHelper $helper, $categoryTitles = [] ) {
 		return [];
 	}
+
 	/**
 	 *
 	 * @param CategoryLinksHelper $helper
@@ -53,35 +26,4 @@ class AddCategories extends SetCategories {
 	protected function getCategoriesToAdd( CategoryLinksHelper $helper, $categoryTitles = [] ) {
 		return $categoryTitles;
 	}
-
-	/**
-	 * @return array|ParamDefinition[]
-	 */
-	public function getArgsDefinitions() {
-		$args = parent::getArgsDefinitions();
-		$pageTitle = new ParamDefinition(
-			ParamType::STRING,
-			static::PARAM_PAGE_TITLE,
-			[],
-			null,
-			false
-		);
-		$args[] = $pageTitle;
-
-		return $args;
-	}
-
-	/**
-	 * @return WikiCategoryPage
-	 */
-	protected function getWikiPage() {
-		if ( !$this->wikiCategoryPage ) {
-			$pageTitle = $this->getParam( 'page_title' );
-			$category = Category::newFromName( $pageTitle );
-			$this->wikiCategoryPage = WikiPage::factory( $category->getTitle() );
-		}
-
-		return $this->wikiCategoryPage;
-	}
-
 }
