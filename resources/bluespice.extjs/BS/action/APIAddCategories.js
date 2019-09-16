078578dcf360fb6ce6f5071dec0eb7c3bf44e707
oleksandrpinchuk <intracomof@gmail.com>
2019-09-16 18:22:08 +0300

    Request addCategories task with context
    Related changes  https://gerrit.wikimedia.org/r/#/c/mediawiki/extensions/BlueSpiceFoundation/+/536638/
    
    ERM 16382
    
    Change-Id: I98936a04e450224024c68e902b76746a55f75be8

--- resources/bluespice.extjs/BS/action/APIAddCategories.js
+++ resources/bluespice.extjs/BS/action/APIAddCategories.js
@@ -20,30 +20,47 @@ Ext.define( 'BS.action.APIAddCategories', {
 	doAPIAddCategories: function( dfd, set ){
 		var me = this;
 
+		var t = mw.Title.newFromText( set.page_title );
+
+		var cnt = {
+			wgAction: 'edit',
+			wgCanonicalNamespace: t.getNamespacePrefix(),
+			wgCanonicalSpecialPageName: false,
+			wgNamespaceNumber: t.getNamespaceId(),
+			wgPageName: t.getPrefixedText(),
+			wgRedirectedFrom: null,
+			wgRelevantPageName: t.getPrefixedText(),
+			wgTitle:t.getName()
+		};
+
 		var taskData = {
-			page_title: set.page_title,
+			page_title: t.getPrefixedText(),
 			categories: set.categories
 		};
-		bs.api.tasks.execSilent(
-			'wikipage', 'addCategories', taskData
-		)
-		.fail(function( response ){
+
+		bs.api.tasks.exec(
+			'wikipage',
+			'wikipage-addcategories',
+			taskData,
+			{
+				useService: true,
+				context: cnt
+			}
+		).fail(function( response ){
 			me.actionStatus = BS.action.Base.STATUS_ERROR;
 			dfd.reject( me, set, response);
 		})
-		.done(function( response ) {
-			if( !response.success ){
-				me.actionStatus = BS.action.Base.STATUS_ERROR;
-				dfd.reject( me, set, response );
-			}
-			this.actionStatus = BS.action.Base.STATUS_DONE;
-			dfd.resolve( me );
-		});
+			.done(function( response ) {
+				if( !response.success ){
+					me.actionStatus = BS.action.Base.STATUS_ERROR;
+					dfd.reject( me, set, response );
+				}
+				this.actionStatus = BS.action.Base.STATUS_DONE;
+				dfd.resolve( me );
+			});
 	},
 
 	getDescription: function(){
 		return mw.message( 'bs-deferred-action-apiaddcategories-description', this.pageTitle ).parse();
 	}
 });
-
-
