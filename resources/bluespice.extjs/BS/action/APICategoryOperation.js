2caff66573f175d9704c660b0d662ed606ab1f53
Dejan Savuljesku <d.savuljesku@gmail.com>
2019-09-17 09:00:38 +0200

    Refactor BS.action category operations
    Based on the original commit by Alex Pinchuk
    
    Use new task service for category operation
    
    [3.1]
    
    Change-Id: Icb256ec3985f9a766469b264756fe8a8986c939c
    ERM: #16382

---
+++ resources/bluespice.extjs/BS/action/APICategoryOperation.js
@@ -0,0 +1,80 @@
+Ext.define( 'BS.action.APICategoryOperation', {
+	extend: 'BS.action.Base',
+	categories: [],
+	pageTitle: '',
+	pageId: -1,
+	task: '',
+
+	execute: function(){
+		var dfd = $.Deferred();
+		this.actionStatus = BS.action.Base.STATUS_RUNNING;
+
+		var set = {
+			categories: this.categories,
+			page_title: this.pageTitle
+		};
+
+		this.doAPIAddCategories( dfd, set );
+
+		return dfd.promise();
+	},
+
+	doAPIAddCategories: function( dfd, set ){
+		var me = this;
+
+		var taskData = {
+			categories: set.categories.join( '|' )
+		};
+
+		bs.api.tasks.exec(
+			'wikipage',
+			this.task,
+			taskData,
+			{
+				useService: true,
+				context: this.getContextObject( set )
+			}
+		).fail(function( response ){
+			me.actionStatus = BS.action.Base.STATUS_ERROR;
+			dfd.reject( me, set, response);
+		})
+			.done(function( response ) {
+				if( !response.success ){
+					me.actionStatus = BS.action.Base.STATUS_ERROR;
+					dfd.reject( me, set, response );
+				}
+				this.actionStatus = BS.action.Base.STATUS_DONE;
+				dfd.resolve( me );
+			});
+	},
+
+	getContextObject: function( set ){
+		var title, context = {
+			wgAction: 'edit',
+			wgCanonicalSpecialPageName: false,
+			wgRedirectedFrom: null
+		};
+		if ( set.hasOwnProperty( 'page_id' ) ) {
+			context.wgArticleId = set.page_id;
+			return context;
+		}
+
+		if ( set.hasOwnProperty( 'page_title' ) ) {
+			title = mw.Title.newFromText( set.page_title );
+		}
+
+		context.wgCanonicalNamespace = title.getNamespacePrefix();
+		context.wgPageName = title.getPrefixedText();
+		context.wgNamespaceNumber = title.getNamespaceId();
+		context.wgRedirectedFrom = title.getPrefixedText();
+		context.wgTitle = title.getName();
+
+		return context;
+	},
+
+	getDescription: function() {
+		// STUB
+	}
+});
+
+
