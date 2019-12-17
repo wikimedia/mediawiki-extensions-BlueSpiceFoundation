1925814d4c9e3c225e6fc07dfab333e8b64018d0
rvogel <vogel@hallowelt.biz>
2019-12-17 14:54:25 +0100

    Make Watchlist API store user context sensitive
    ERM17700
    
    [3.1.x]
    
    Change-Id: Iedbe943b5281cd725a074f548a7016cc1f0069f3

--- src/Api/Store/Watchlist.php
+++ src/Api/Store/Watchlist.php
@@ -5,6 +5,6 @@ namespace BlueSpice\Api\Store;
 class Watchlist extends \BlueSpice\Api\Store {
 
 	protected function makeDataStore() {
-		return new \BlueSpice\Data\Watchlist\Store( $this->getContext() );
+		return new \BlueSpice\Data\Watchlist\Store( $this->getContext(), true );
 	}
 }
