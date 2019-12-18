2a8322d40011b9aa7df1ffcf1ea7f8a0c05afb48
pwirth <wirth@hallowelt.biz>
2019-12-18 11:00:54 +0100

    Add EditPage hook handler base classes
    ERM:17711
    [3.1.x]
    
    Change-Id: Ie269b9449746151c1e92f63affb3c454b26a29a9

---
+++ src/Hook/EditPageAttemptSave.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * Hook handler base class for MediaWiki hook EditPage::attemptSave
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ * This file is part of BlueSpice MediaWiki
+ * For further information visit https://bluespice.com
+ *
+ * @author     Patric Wirth
+ * @package    BlueSpiceFoundation
+ * @copyright  Copyright (C) 2019 Hallo Welt! GmbH, All rights reserved.
+ * @license    http://www.gnu.org/copyleft/gpl.html GPL-3.0-only
+ * @filesource
+ */
+namespace BlueSpice\Hook;
+
+use IContextSource;
+use Config;
+use EditPage;
+use BlueSpice\Hook;
+
+abstract class EditPageAttemptSave extends Hook {
+
+	/**
+	 *
+	 * @var EditPage
+	 */
+	protected $editpage = null;
+
+	/**
+	 *
+	 * @param EditPage $editpage
+	 * @return bool
+	 */
+	public static function callback( $editpage ) {
+		$className = static::class;
+		$hookHandler = new $className(
+			$editpage->getContext(),
+			null,
+			$editpage
+		);
+		return $hookHandler->process();
+	}
+
+	/**
+	 *
+	 * @param IContextSource $context
+	 * @param Config $config
+	 * @param EditPage $editpage
+	 */
+	public function __construct( $context, $config, $editpage ) {
+		parent::__construct( $context, $config );
+
+		$this->editpage = $editpage;
+	}
+}
