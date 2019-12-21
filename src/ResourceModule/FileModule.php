6600c26ddb6467d16753881686679ed4e45ee7c9
rvogel <vogel@hallowelt.biz>
2019-12-21 22:53:12 +0100

    Add base classe for RL file modules
    Combines "Template" and "LessVars"
    
    [3.2]
    
    Change-Id: Ic0d6b430ac57810983466cd35d1d6a7a041dd8e0

---
+++ src/ResourceModule/FileModule.php
@@ -0,0 +1,97 @@
+<?php
+/**
+ * ResourceLoader class for manifest RL file modules for BlueSpice
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ * This file is part of BlueSpice MediaWiki
+ * For further information visit https://bluespice.com
+ *
+ * @author     Patric Wirth, Robert Vogel
+ * @package    BlueSpiceFoundation
+ * @copyright  Copyright (C) 2019 Hallo Welt! GmbH, All rights reserved.
+ * @license    http://www.gnu.org/copyleft/gpl.html GPL-3.0-only
+ */
+namespace BlueSpice\ResourceModule;
+
+use ResourceLoaderFileModule;
+use BlueSpice\Services;
+use BlueSpice\TemplateFactory;
+
+class FileModule extends ResourceLoaderFileModule {
+
+	/**
+	 * Takes named templates by the module and returns an array mapping.
+	 * @return array Templates mapping template alias to content
+	 * @throws MWException
+	 */
+	public function getTemplates() {
+		$templates = [];
+		foreach ( $this->templates as $alias => $templatePath ) {
+			// Alias is optional
+			if ( is_int( $alias ) ) {
+				$alias = $templatePath;
+			}
+			$template = $this->getTemplateFactory()->get( $alias );
+			$localPath = $template->getFilePath();
+			if ( file_exists( $localPath ) ) {
+				$content = file_get_contents( $localPath );
+				$templates[$alias] = $this->stripBom( $content );
+			} else {
+				$msg = __METHOD__ . ": template file not found: \"$localPath\"";
+				wfDebugLog( 'resourceloader', $msg );
+				throw new \MWException( $msg );
+			}
+		}
+		return $templates;
+	}
+
+	/**
+	 *
+	 * @return TemplateFactory
+	 */
+	protected function getTemplateFactory() {
+		return Services::getInstance()->getBSTemplateFactory();
+	}
+
+	/**
+	 *
+	 * @param \ResourceLoaderContext $context
+	 * @return array
+	 */
+	public function getLessVars( \ResourceLoaderContext $context ) {
+		$vars = parent::getLessVars( $context );
+		$registry = new \BlueSpice\ExtensionAttributeBasedRegistry(
+			'BlueSpiceFoundationLessVarsRegistry'
+		);
+		foreach ( $registry->getAllKeys() as $key ) {
+			$vars[$key] = $registry->getValue( $key, '¯\_(ツ)_/¯' );
+		}
+
+		// TODO: make LessVars text area field with valiadtion for ConfigManager
+		return array_merge(
+			$vars,
+			$this->getConfig()->get( 'LessVars' )
+		);
+	}
+
+	/**
+	 * @return Config
+	 * @since 1.24
+	 */
+	public function getConfig() {
+		return \BlueSpice\Services::getInstance()->getConfigFactory()
+			->makeConfig( 'bsg' );
+	}
+}
