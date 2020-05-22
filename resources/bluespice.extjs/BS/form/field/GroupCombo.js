0aab87705cc20cfd294066d29d30e966a7ff3a37
rvogel <vogel@hallowelt.biz>
2020-05-22 14:04:23 +0200

    Add ExtJS form fields for user-groups
    * Also remove stub
    
    ERM18764
    [3.1.x]
    
    Change-Id: I9c8fc360019d8fbf65669db1fdde1b22e54c6c64

---
+++ resources/bluespice.extjs/BS/form/field/GroupCombo.js
@@ -0,0 +1,22 @@
+Ext.define( 'BS.form.field.GroupCombo', {
+	extend: 'Ext.form.field.ComboBox',
+	requires: [ 'BS.store.ApiGroup' ],
+	triggerAction: 'all',
+	displayField: 'displayname',
+	valueField: 'group_name',
+	allowBlank: false,
+	forceSelection: true,
+	queryMode: 'local',
+	fieldLabel: mw.message( 'bs-extjs-label-group' ).plain(),
+	emptyText: mw.message( "bs-extjs-combo-box-default-placeholder" ).plain(),
+
+	initComponent: function() {
+		this.store = this.makeStore();
+		this.callParent(arguments);
+	},
+
+	makeStore: function() {
+		return new BS.store.ApiGroup();
+	},
+});
+
