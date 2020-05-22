0709dece2b57168b064ce30333e4f22d2d902e24
rvogel <vogel@hallowelt.biz>
2020-05-22 14:04:23 +0200

    Add ExtJS form fields for user-groups
    * Also remove stub
    
    ERM18764
    [3.1.x]
    
    Change-Id: I9c8fc360019d8fbf65669db1fdde1b22e54c6c64

--- resources/bluespice.extjs/BS/form/field/GroupTag.js
+++ resources/bluespice.extjs/BS/form/field/GroupTag.js
@@ -1,7 +1,15 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+Ext.define( 'BS.form.field.GroupTag', {
+	extend:'Ext.form.field.Tag',
+	requires: [ 'BS.store.ApiGroup' ],
+	displayField: 'displayname',
+	valueField: 'group_name',
 
+	initComponent: function() {
+		this.store = this.makeStore();
+		this.callParent( arguments );
+	},
 
+	makeStore: function() {
+		return new BS.store.ApiGroup();
+	},
+});
---
+++ resources/bluespice.extjs/BS/store/ApiGroup.js
@@ -0,0 +1,9 @@
+Ext.define('BS.store.ApiGroup', {
+	extend: 'BS.store.BSApi',
+	apiAction: 'bs-group-store',
+	fields: [
+		'group_name',
+		'additional_group',
+		'displayname'
+	]
+});
