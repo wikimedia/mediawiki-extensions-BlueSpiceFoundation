c52fd8ca14e69209f7d76336fee1859e2dec67f5
pwirth <wirth@hallowelt.biz>
2020-07-18 12:02:13 +0200

    Remove 'wg' global usage from config files
    All current config files already use $GLOBALS but this also provides a
    migration for older versions
    
    ERM:19636
    [3.3]
    
    Change-Id: Iada193135c0c371217a19517309e792afb3d68a1

--- src/Hook/SetupAfterCache/AddConfigFiles.php
+++ src/Hook/SetupAfterCache/AddConfigFiles.php
@@ -5,20 +5,6 @@ namespace BlueSpice\Hook\SetupAfterCache;
 class AddConfigFiles extends \BlueSpice\Hook\SetupAfterCache {
 
 	protected function doProcess() {
-		// phpcs:ignore MediaWiki.VariableAnalysis.UnusedGlobalVariables
-		global $wgExtensionFunctions, $wgGroupPermissions, $wgWhitelistRead, $wgMaxUploadSize;
-		// phpcs:ignore MediaWiki.VariableAnalysis.UnusedGlobalVariables
-		global $wgNamespacePermissionLockdown, $wgSpecialPageLockdown, $wgActionLockdown;
-		// phpcs:ignore MediaWiki.VariableAnalysis.UnusedGlobalVariables
-		global $wgNonincludableNamespaces, $wgExtraNamespaces, $wgContentNamespaces;
-		// phpcs:ignore MediaWiki.VariableAnalysis.UnusedGlobalVariables
-		global $wgNamespacesWithSubpages, $wgNamespacesToBeSearchedDefault, $wgLocalisationCacheConf;
-		// phpcs:ignore MediaWiki.VariableAnalysis.UnusedGlobalVariables
-		global $wgAutoloadLocalClasses, $wgFlaggedRevsNamespaces, $wgNamespaceAliases, $wgVersion;
-		/*
-		 * TODO: All those globals above can be removed once all included
-		 * settings files use $GLOBALS['wg...'] to access them
-		 */
 		foreach ( $this->getConfig()->get( 'ConfigFiles' ) as $sConfigFileKey => $sConfigFilePath ) {
 			if ( file_exists( $sConfigFilePath ) ) {
 				include $sConfigFilePath;
