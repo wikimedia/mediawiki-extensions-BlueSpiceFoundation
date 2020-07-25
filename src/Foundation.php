82c5fc105821122931bcc5d85a90981b2f4cc099
Dejan Savuljesku <d.savuljesku@gmail.com>
2020-07-25 17:16:27 +0200

    Use lib for LessVars
    [master]
    
    Change-Id: I28b8f6d5b7c3522f55fd06cd6101646b0d7f1b6b

--- src/Foundation.php
+++ src/Foundation.php
@@ -2,6 +2,9 @@
 
 namespace BlueSpice;
 
+use MediaWiki\MediaWikiServices;
+use MWStake\MediaWiki\Component\CommonUserInterface\LessVars;
+
 class Foundation {
 
 	/**
@@ -42,6 +45,58 @@ class Foundation {
 				$foundation->initialize();
 			}
 		);
+
+		static::setLessVars();
+	}
+
+	/**
+	 * Set less variables
+	 */
+	protected static function setLessVars() {
+		$lessVars = LessVars::getInstance();
+
+		// Set defaults
+		$lessVars->setVar( 'primary-bg', '#3e5389' );
+		$lessVars->setVar( 'secondary-bg', '#ffae00' );
+		$lessVars->setVar( 'neutral-bg', '#929292' );
+
+		$lessVars->setVar( 'bs-color-primary', '@primary-bg' );
+		$lessVars->setVar( 'bs-color-secondary', '@secondary-bg' );
+		$lessVars->setVar( 'bs-color-tertiary', '#b73a3a' );
+		$lessVars->setVar( 'bs-color-neutral', '@neutral-bg' );
+		$lessVars->setVar( 'bs-color-neutral2', '#ABABAB' );
+		$lessVars->setVar( 'bs-color-neutral3', '#C4C4C4' );
+		$lessVars->setVar( 'bs-color-neutral4', '#787878' );
+		$lessVars->setVar( 'bs-color-secondary-information', 'darken( @neutral-bg, 17.3% )' );
+		$lessVars->setVar( 'bs-color-progressive', '#347bff' );
+		$lessVars->setVar( 'bs-color-constructive', '#00af89' );
+		$lessVars->setVar( 'bs-color-destructive', '#d11d13' );
+		$lessVars->setVar( 'bs-color-success', '#dff0d8' );
+		$lessVars->setVar( 'bs-color-warning', '#fcf8e3' );
+		$lessVars->setVar( 'bs-color-error', '#f2dede' );
+		$lessVars->setVar( 'bs-color-info', '#d9edf7' );
+		$lessVars->setVar( 'bs-background-neutral', '#FFFFFF' );
+		$lessVars->setVar( 'bs-background-primary', 'none' );
+		$lessVars->setVar( 'bs-loading-indicator-color', '#ffae00' );
+		$lessVars->setVar( 'bs-color-link', '#0060DF' );
+		$lessVars->setVar( 'bs-color-link-hover', '#006EFF' );
+		$lessVars->setVar( 'bs-color-link-new', '#B73A3A' );
+		$lessVars->setVar( 'bs-color-link-new-hover', '#E92121' );
+
+		// Override with values from registry - legacy
+		$registry = new \BlueSpice\ExtensionAttributeBasedRegistry(
+			'BlueSpiceFoundationLessVarsRegistry'
+		);
+
+		foreach ( $registry->getAllKeys() as $key ) {
+			$lessVars->setVar( $key, $registry->getValue( $key ) );
+		}
+
+		// Override with values from config - legacy
+		$config = MediaWikiServices::getInstance()->getConfigFactory()->makeConfig( 'bsg' );
+		foreach ( $config->get( 'LessVars' ) as $key => $value ) {
+			$lessVars->setVar( $key, $value );
+		}
 	}
 
 	private function __construct() {
--- src/ResourceModule/LessVars.php
+++ src/ResourceModule/LessVars.php
@@ -2,34 +2,10 @@
 
 namespace BlueSpice\ResourceModule;
 
-class LessVars extends \ResourceLoaderFileModule {
-	/**
-	 *
-	 * @param \ResourceLoaderContext $context
-	 * @return array
-	 */
-	public function getLessVars( \ResourceLoaderContext $context ) {
-		$vars = parent::getLessVars( $context );
-		$registry = new \BlueSpice\ExtensionAttributeBasedRegistry(
-			'BlueSpiceFoundationLessVarsRegistry'
-		);
-		foreach ( $registry->getAllKeys() as $key ) {
-			$vars[$key] = $registry->getValue( $key, '¯\_(ツ)_/¯' );
-		}
+use MWStake\MediaWiki\Component\CommonUserInterface\ResourceLoader\LessVars as LessVarsLib;
 
-		// TODO: make LessVars text area field with valiadtion for ConfigManager
-		return array_merge(
-			$vars,
-			$this->getConfig()->get( 'LessVars' )
-		);
-	}
-
-	/**
-	 * @return Config
-	 * @since 1.24
-	 */
-	public function getConfig() {
-		return \BlueSpice\Services::getInstance()->getConfigFactory()
-			->makeConfig( 'bsg' );
-	}
+/**
+ * @deprecated Since 3.3 Use MWStake\MediaWiki\Component\CommonUserInterface\ResourceLoader\LessVars instead
+ */
+class LessVars extends LessVarsLib {
 }
