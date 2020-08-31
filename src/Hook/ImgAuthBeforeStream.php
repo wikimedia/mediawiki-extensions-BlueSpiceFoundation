d02c6162c4fcd0a9c3105b3d5de42544f217d254
rvogel <vogel@hallowelt.biz>
2020-08-31 12:58:56 +0200

    Fix file header for ImgAuthBeforeStream
    ERM19529
    
    [3.1.x]
    
    Change-Id: Ibadac432a80c89e2f4d23a28d3449b63cff3b410

--- src/Hook/ImgAuthBeforeStream.php
+++ src/Hook/ImgAuthBeforeStream.php
@@ -1,11 +1,29 @@
 <?php
-
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * Hook handler base class for MediaWiki hook ImgAuthBeforeStream
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ * This file is part of BlueSpice MediaWiki
+ * For further information visit https://bluespice.com
+ *
+ * @author     Patric Wirth
+ * @package    BlueSpiceFoundation
+ * @copyright  Copyright (C) 2020 Hallo Welt! GmbH, All rights reserved.
+ * @license    http://www.gnu.org/copyleft/gpl.html GPL-3.0-only
+ * @filesource
  */
-
 namespace BlueSpice\Hook;
 
 use BlueSpice\Hook;
