2b070aeaa4997bbc6741ab83bae967ee8ba6489e
pwirth <wirth@hallowelt.biz>
2020-09-03 13:59:39 +0200

    Remove deprecated since 3.0 BsDebug
    ERM:20733
    [3.3]
    
    Change-Id: I51c95555b0d95ecd8043f620ff26d5bd9f99209f

--- includes/Debug.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-/**
- * DEPRECATED!
- * @deprecated since version 3.0.1 - use the global functions bsDebug* or
- * \BlueSpice\Debug instead
- */
-class BSDebug extends \BlueSpice\Debug {
-}
