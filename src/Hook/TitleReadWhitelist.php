6c6fdfc6aae88a84e735b2fa1366ebb85f314a40
pwirth <wirth@hallowelt.biz>
2020-10-17 14:57:37 +0200

    Add hookhandler base class for TitleReadWhitelist
    ERM:10378
    [3.1.x]
    
    Change-Id: I26260392fde44ce08bab7f24536dd9ac9bd89311

---
+++ src/Hook/TitleReadWhitelist.php
@@ -0,0 +1,89 @@
+<?php
+/**
+ * Hook handler base class for MediaWiki hook TitleReadWhitelist
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ * This file is part of BlueSpice MediaWiki
+ * For further information visit https://bluespice.com
+ *
+ * @author     Patric Wirth
+ * @package    BlueSpiceFoundation
+ * @copyright  Copyright (C) 2020 Hallo Welt! GmbH, All rights reserved.
+ * @license    http://www.gnu.org/copyleft/gpl.html GPL-3.0-only
+ * @filesource
+ */
+namespace BlueSpice\Hook;
+
+use BlueSpice\Hook;
+use Config;
+use IContextSource;
+use Title;
+use User;
+
+abstract class TitleReadWhitelist extends Hook {
+
+	/**
+	 *
+	 * @var Title
+	 */
+	protected $title = null;
+
+	/**
+	 *
+	 * @var User
+	 */
+	protected $user = null;
+
+	/**
+	 *
+	 * @var bool
+	 */
+	protected $whitelisted = false;
+
+	/**
+	 *
+	 * @param Title &$title
+	 * @param User &$user
+	 * @param bool &$whitelisted
+	 * @return bool
+	 */
+	public static function callback( &$title, &$user, &$whitelisted ) {
+		$className = static::class;
+		$hookHandler = new $className(
+			null,
+			null,
+			$title,
+			$user,
+			$whitelisted
+		);
+		return $hookHandler->process();
+	}
+
+	/**
+	 *
+	 * @param IContextSource $context
+	 * @param Config $config
+	 * @param Title &$title
+	 * @param User &$user
+	 * @param bool &$whitelisted
+	 */
+	public function __construct( $context, $config, &$title, &$user, &$whitelisted ) {
+		parent::__construct( $context, $config );
+
+		$this->title = $title;
+		$this->user = $user;
+		$this->whitelisted =& $whitelisted;
+	}
+}
