661a6f79096480acc014d1baa55560eb05466a3f
pwirth <wirth@hallowelt.biz>
2020-11-04 18:02:44 +0100

    Add hook handler base class for hook SidebarBeforeOutput
    ERM:19636
    [3.1.x]
    
    Change-Id: Ieeaec402bfb511a517ac0152e0a5ff3e860e9e39

---
+++ src/Hook/SidebarBeforeOutput.php
@@ -0,0 +1,81 @@
+<?php
+
+/**
+ * Hook handler base class for MediaWiki hook SidebarBeforeOutput
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ * This file is part of BlueSpice MediaWiki
+ * For further information visit https://bluespice.com
+ *
+ * @author     Patric Wirth
+ * @package    BlueSpiceFoundation
+ * @copyright  Copyright (C) 2020 Hallo Welt! GmbH, All rights reserved.
+ * @license    http://www.gnu.org/copyleft/gpl.html GPL-3.0-only
+ * @filesource
+ */
+
+namespace BlueSpice\Hook;
+
+use BlueSpice\Hook;
+use Config;
+use IContextSource;
+use Skin;
+
+abstract class SidebarBeforeOutput extends Hook {
+
+	/**
+	 *
+	 * @var Skin
+	 */
+	protected $skin = null;
+
+	/**
+	 * An array of sidebar items
+	 * @var array
+	 */
+	protected $sidebar = [];
+
+	/**
+	 *
+	 * @param Skin $skin
+	 * @param array &$sidebar
+	 * @return bool
+	 */
+	public static function callback( $skin, &$sidebar ) {
+		$className = static::class;
+		$hookHandler = new $className(
+			null,
+			null,
+			$skin,
+			$sidebar
+		);
+		return $hookHandler->process();
+	}
+
+	/**
+	 *
+	 * @param IContextSource $context
+	 * @param Config $config
+	 * @param Skin $skin
+	 * @param array &$sidebar
+	 */
+	public function __construct( $context, $config, $skin, &$sidebar ) {
+		parent::__construct( $context, $config );
+
+		$this->skin = $skin;
+		$this->sidebar =& $sidebar;
+	}
+
+}
