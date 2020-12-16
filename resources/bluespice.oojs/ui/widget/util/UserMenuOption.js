aa2a0e7b6482a94df91f463d479179ddb66e5889
bluespicebot <savuljesku@hallowelt.com>
2020-12-16 10:12:07 +0100

    Add OOJS user picker
    Also, add user image link to bs-user-store
    
    [3.1.x]
    
    ERM 22011
    
    Change-Id: I1214b2e805afed8447931781cef1c9d7d76a6120

---
+++ resources/bluespice.oojs/ui/widget/util/UserMenuOption.js
@@ -0,0 +1,27 @@
+( function( mw, $, bs ) {
+	bs.ui.widget.util.UserMenuOptionWidget = function( user ) {
+		var config = {};
+
+		config.data = user;
+		bs.ui.widget.util.UserMenuOptionWidget.parent.call( this, config );
+
+		this.$element.children().remove();
+		if ( user.user_image !== '' ) {
+			var $userImage = $( user.user_image );
+			this.$element.append( $userImage.find( 'img' ).addClass( 'user-image' ) );
+		}
+
+		var $nameBox = $( '<div>' ).addClass( 'user-name-cnt' );
+		$nameBox.append($( '<span>' ).addClass( 'user-display' ).text( user.display_name ) );
+		if ( user.display_name !== user.user_name ) {
+			$nameBox.append($( '<span>' ).addClass( 'user-username' ).text( user.user_name) );
+		}
+
+		this.$element.append( $nameBox );
+		this.$element.addClass( 'bs-ui-widget-UserPicker-menu-option' );
+		$( '.bs-banner' ).append( this.$element );
+	};
+
+	OO.inheritClass( bs.ui.widget.util.UserMenuOptionWidget, OO.ui.MenuOptionWidget );
+
+} ) ( mediaWiki, jQuery, blueSpice );
