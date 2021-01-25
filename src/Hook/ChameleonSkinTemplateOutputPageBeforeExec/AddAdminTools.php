1056366110fa761522ff0c1c3073e67bf1ca7568
Dejan Savuljesku <savuljesku@hallowelt.com>
2021-01-25 11:54:37 +0100

    Add Hook handler base class for ChameleonSkinTemplateOutputPageBeforeExec
    Moved from BlueSpiceCalumma to break Foundation's dependeny on it
    
    [4.0]
    
    ERM 22695
    
    Change-Id: Ie953704861cb53677a954543953a9a1ad82ce4ab

--- src/Hook/ChameleonSkinTemplateOutputPageBeforeExec/AddAdminTools.php
+++ src/Hook/ChameleonSkinTemplateOutputPageBeforeExec/AddAdminTools.php
@@ -2,7 +2,7 @@
 
 namespace BlueSpice\Hook\ChameleonSkinTemplateOutputPageBeforeExec;
 
-use BlueSpice\Calumma\Hook\ChameleonSkinTemplateOutputPageBeforeExec;
+use BlueSpice\Hook\ChameleonSkinTemplateOutputPageBeforeExec;
 use BlueSpice\SkinData;
 
 class AddAdminTools extends ChameleonSkinTemplateOutputPageBeforeExec {
