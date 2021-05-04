489ea9f0047e25a021546cf337b24ada22959bb9
rvogel <vogel@hallowelt.biz>
2021-05-04 12:09:18 +0200

    Add test maintenance script for manifest attributes
    Example usage:
    
    php extensions/BlueSpiceFoundation/maintenance/InspectManifestAttributes.php --attributeName=BlueSpiceFoundationRunJobsTriggerHandlerRegistry
    
    ERM24179
    
    [3.2.x]
    
    Change-Id: I08641a2835fff3877e96a6d3d8ef865073de7c1b

---
+++ maintenance/InspectManifestAttributes.php
@@ -0,0 +1,27 @@
+<?php
+
+require_once 'BSMaintenance.php';
+
+class InspectManifestAttributes extends BSMaintenance {
+
+	public function __construct() {
+		parent::__construct();
+
+		$this->addOption(
+			'attributeName',
+			'Name of the attribute to check',
+			true,
+			true
+		);
+	}
+
+	public function execute() {
+		$attributeName = $this->getOption( 'attributeName' );
+		$data = ExtensionRegistry::getInstance()->getAttribute( $attributeName );
+		$jsonString = FormatJson::encode( $data, true );
+		$this->output( $jsonString );
+	}
+}
+
+$maintClass = 'InspectManifestAttributes';
+require_once RUN_MAINTENANCE_IF_MAIN;
