2d79e22f4c37a65c1f626a57bde57e3c6763a779
Pwirth <wirth@hallowelt.biz>
2021-10-13 11:33:31 +0000

    Add hook handler base class for hook EditPage::showEditForm:initial
    ERM10378
    
    [3.2.x]
    
    Change-Id: Id70111fc3a984031d13e1d13c64e1639532b8c3c

---
+++ src/Hook/EditPageShowEditFormInitial.php
@@ -0,0 +1,79 @@
+<?php
+/**
+ * Hook handler base class for MediaWiki hook EditPage::showEditForm:initial
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ * This file is part of BlueSpice MediaWiki
+ * For further information visit https://bluespice.com
+ *
+ * @author     Patric Wirth
+ * @package    BlueSpiceFoundation
+ * @copyright  Copyright (C) 2021 Hallo Welt! GmbH, All rights reserved.
+ * @license    http://www.gnu.org/copyleft/gpl.html GPL-3.0-only
+ * @filesource
+ */
+namespace BlueSpice\Hook;
+
+use BlueSpice\Hook;
+use Config;
+use EditPage;
+use IContextSource;
+use OutputPage;
+
+abstract class EditPageShowEditFormInitial extends Hook {
+
+	/**
+	 *
+	 * @var EditPage
+	 */
+	protected $editpage = null;
+
+	/**
+	 *
+	 * @var OutputPage
+	 */
+	protected $output = null;
+
+	/**
+	 *
+	 * @param EditPage $editpage
+	 * @param OutputPage $output
+	 * @return bool
+	 */
+	public static function callback( $editpage, $output ) {
+		$className = static::class;
+		$hookHandler = new $className(
+			$editpage->getContext(),
+			null,
+			$editpage,
+			$output
+		);
+		return $hookHandler->process();
+	}
+
+	/**
+	 *
+	 * @param IContextSource $context
+	 * @param Config $config
+	 * @param EditPage $editpage
+	 * @param OutputPage $output
+	 */
+	public function __construct( $context, $config, $editpage, $output ) {
+		parent::__construct( $context, $config );
+
+		$this->editpage = $editpage;
+		$this->output = $output;
+	}
+}
