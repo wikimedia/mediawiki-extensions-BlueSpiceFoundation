8508ece5cd2c142787ed9c3548cea97ec235c61e
akulbii <alk@astarios.com>
2021-11-20 16:16:42 +0200

    Add empty API formatter
    It may be useful for some specific API cases, like attachment download
    
    [3.2.x]
    
    ERM26154
    
    Change-Id: I6cf8ddc15311b26d4981b7f1491c4526db4e5209

---
+++ src/Api/Format/None.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace BlueSpice\Api\Format;
+
+use ApiFormatBase;
+
+/**
+ * API formatter which is used for situations, when something really custom should be outputted.
+ */
+class None extends ApiFormatBase {
+
+	/**
+	 * @inheritDoc
+	 */
+	public function __construct() {
+		// Nothing to do here
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function initPrinter( $unused = false ) {
+		// Nothing to do here, thus output should be empty
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function execute() {
+		// Nothing to do here, thus output should be empty
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function closePrinter() {
+		// Nothing to do here, thus output should be empty
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function getMimeType() {
+		// MIME type is not used, so nothing to do here
+	}
+}
