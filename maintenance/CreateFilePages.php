5a17c043e7ccfc24db11b4adbd18e1a32628c199
akulbii <alk@astarios.com>
2022-02-02 14:08:30 +0200

    Add maintenance script for creating file pages
    There could be cases when file exists in wiki, but it does not have corresponding wiki page.
    This maintenance script fixes such cases by creating corresponding wiki page.
    
    [3.2.x]
    
    ERM20742
    
    Change-Id: I54f4ba47b762124d403c50b23b24f57ee1b527d1
    (cherry picked from commit a52914b6984dabab06cae37e0176c464d5972387)
    (cherry picked from commit 09014b6160798bc28d59fbee28c596be724ba1ca)
    (cherry picked from commit 4331a5c42c9909956c74595095cae7d70b5aabfa)

---
+++ maintenance/CreateFilePages.php
@@ -0,0 +1,75 @@
+<?php
+
+require_once dirname( dirname( dirname( __DIR__ ) ) ) . "/maintenance/Maintenance.php";
+
+class CreateFilePages extends Maintenance {
+
+	/**
+	 * @inheritDoc
+	 */
+	public function __construct() {
+		parent::__construct();
+		$this->addOption( 'dry', 'Just outputs problematic pages without actual changes in DB' );
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function execute() {
+		$dbr = $this->getDB( DB_REPLICA );
+
+		$res = $dbr->select(
+			[
+				'image',
+				'page'
+			],
+			'img_name',
+			[
+				'page_id IS NULL'
+			],
+			__METHOD__,
+			[],
+			[
+				'page' => [
+					'LEFT JOIN',
+					[ 'img_name = page_title' ]
+				]
+			]
+		);
+
+		if ( $res->numRows() > 0 ) {
+			$this->output( 'Some files do not have corresponding wiki page yet:' . PHP_EOL );
+		} else {
+			$this->output( 'All files have corresponding pages.' . PHP_EOL );
+			return;
+		}
+
+		foreach ( $res as $row ) {
+			$this->output( $row->img_name . PHP_EOL );
+
+			if ( $this->getOption( 'dry' ) ) {
+				continue;
+			}
+
+			$title = Title::makeTitle( NS_FILE, $row->img_name );
+			$wikipage = WikiPage::factory( $title );
+
+			$text = '-';
+			$comment = 'Restore file description page';
+
+			$content = ContentHandler::makeContent( $text, $title );
+			$status = $wikipage->doEditContent( $content, $comment );
+
+			if ( $status->isGood() ) {
+				$this->output( 'Page created successfully...' . PHP_EOL );
+			} else {
+				$this->output( 'Page was not created due to some error!' . PHP_EOL );
+			}
+		}
+
+		$this->output( 'Done!' . PHP_EOL );
+	}
+}
+
+$maintClass = 'CreateFilePages';
+require_once RUN_MAINTENANCE_IF_MAIN;
