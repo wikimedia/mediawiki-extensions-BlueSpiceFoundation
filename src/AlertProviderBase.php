483df8e886edd1af79cae09e73f227d6285e9ded
rvogel <vogel@hallowelt.biz>
2022-05-09 09:49:25 +0200

    Fix MWStake AlertBanners issue
    Introduced by [1]. BlueSpice implementation need a different Config
    object!
    
    [1] https://github.com/hallowelt/mwstake-mediawiki-component-alertbanners/commit/fe6ccd268b23ef9135b1f3846d19cde5018f8d9c
    
    [4.1.x]
    
    ERM28561
    
    Change-Id: If5fd394f7b46d797b8e6c7a624fbbd327febd936

--- src/AlertProviderBase.php
+++ src/AlertProviderBase.php
@@ -2,8 +2,23 @@
 
 namespace BlueSpice;
 
+use MediaWiki\MediaWikiServices;
+use MultiConfig;
 use MWStake\MediaWiki\Component\AlertBanners\AlertProviderBase as AlertBannersAlertProviderBase;
 
 abstract class AlertProviderBase extends AlertBannersAlertProviderBase {
+
+	/**
+	 * @inheritDoc
+	 */
+	public function __construct( $skin, $loadBalancer, $config ) {
+		parent::__construct( $skin, $loadBalancer, $config );
+
+		$bsgConfig = MediaWikiServices::getInstance()->getConfigFactory()->makeConfig( 'bsg' );
+		$this->config = new MultiConfig( [
+			$bsgConfig,
+			$this->config
+		] );
+	}
 
 }
