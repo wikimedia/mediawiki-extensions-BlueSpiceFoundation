39ab2dc5832d084a54347f653767e5508eb21843
d.savuljesku <d.savuljesku@gmail.com>
2022-07-07 15:16:29 +0200

    Yet another compatibility fix
    This time, some implementations expect output of Reader::read() to be BS\Data\RecordSet
    
    [4.2]
    
    ERM 29081
    
    Change-Id: Iafa6fcd2a4c6c690a2d2094e433f67dd4238ced0

--- src/Data/DatabaseReader.php
+++ src/Data/DatabaseReader.php
@@ -8,4 +8,16 @@ use MWStake\MediaWiki\Component\DataStore\DatabaseReader as Base;
  * @deprecated since 4.2. Use mediawiki-component-datastore
  */
 abstract class DatabaseReader extends Base implements IReader {
+	/**
+	 * Compatibility layer - some implementations expect return
+	 * to be the deprecated class
+	 *
+	 * @param ReaderParams $params
+	 *
+	 * @return ResultSet
+	 */
+	public function read( $params ) {
+		$resultSet = parent::read( $params );
+		return new ResultSet( $resultSet->getRecords(), $resultSet->getTotal() );
+	}
 }
