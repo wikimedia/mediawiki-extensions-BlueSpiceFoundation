8d1722c301ab0c898088dd57328d78c00d73b504
Hamish Slater <hamishslater@hotmail.com>
2022-08-30 11:58:48 +0200

    Fix deprecated calls to 'User::setpassword'
    ERM29670
    
    [4.x]
    
    Change-Id: Ia5f31f87890980f561fd6205eecb2c2178b98581

--- maintenance/BSImportUsers.php
+++ maintenance/BSImportUsers.php
@@ -85,7 +85,7 @@ class BSImportUsers extends BSMaintenance {
 
 			$sUserPassword = $this->getOption( 'defaultpw', '' );
 			if ( !empty( $sUserPassword ) ) {
-				$oUser->setPassword( $sUserPassword );
+				$oUser->changeAuthenticationData( [ 'password' => $sUserPassword ] );
 				$oUser->saveSettings();
 			}
 
