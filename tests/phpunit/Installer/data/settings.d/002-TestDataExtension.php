5ca674270e8f12357bcbf7b2df1c093d335d60c6
akulbii <alk@astarios.com>
2022-11-03 13:42:51 +0200

    Move BsAutoExtensionHandler to the BlueSpiceFoundation
    It turned out that BsAutoExtensionHandler, which is used in BlueSpice installer, also should be used in BlueSpiceFarm instance installer.
    That's why it was decided to move it to BlueSpiceFoundation.
    
    [4.2.x]
    
    ERM30228
    
    Change-Id: I3408bba0e34c1470769e8a74d3f5b442104d8403

---
+++ tests/phpunit/Installer/data/settings.d/002-TestDataExtension.php
@@ -0,0 +1,11 @@
+<?php
+
+wfLoadExtension( 'Extension6' );
+// wfLoadExtension( 'Extension7' );
+wfLoadExtension("Extension8");
+#wfLoadExtension( 'Extension9' );
+wfLoadExtension( "Extension10" );
+wfLoadExtension( "Extension11");
+
+return;
+wfLoadExtension( 'Extension14' );
