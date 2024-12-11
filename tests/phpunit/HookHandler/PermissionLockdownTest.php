adee9f20701ce3ee6c40062e2f045a7579f63c33
Robert Vogel <vogel@hallowelt.biz>
2024-12-11 15:39:15 +0100

    Add permission check on API
    Some extensions (e.g. Extension:TextExtracts) may bypass the regular
    permission check.
    
    ERM39926
    
    [4.5.x]
    
    Change-Id: I86221ecdd1d261397d150e106f57efb0a5e386df

--- tests/phpunit/HookHandler/PermissionLockdownTest.php
+++ tests/phpunit/HookHandler/PermissionLockdownTest.php
@@ -2,14 +2,20 @@
 
 namespace BlueSpice\Tests\HookHandler;
 
+use ApiBase;
 use BlueSpice\HookHandler\PermissionLockdown;
 use BlueSpice\Utility\MaintenanceUser;
 use BlueSpice\UtilityFactory;
 use HashConfig;
 use MediaWiki\Linker\LinkTarget;
+use MediaWiki\MediaWikiServices;
 use MediaWiki\Permissions\PermissionManager;
+use MediaWiki\Revision\RevisionLookup;
+use MediaWiki\Revision\RevisionRecord;
 use PHPUnit\Framework\TestCase;
+use TitleFactory;
 use User;
+use WebRequest;
 
 class PermissionLockdownTest extends TestCase {
 	/**
@@ -39,6 +45,8 @@ class PermissionLockdownTest extends TestCase {
 				return $userCanReturn;
 			} );
 
+		$titleFactory = $this->createMock( TitleFactory::class );
+		$revisionLookup = $this->createMock( RevisionLookup::class );
 		$utilityFactory = $this->createMock( UtilityFactory::class );
 		$maintenanceUser = $this->createMock( MaintenanceUser::class );
 		$maintenanceUserUser = $this->createMock( User::class );
@@ -46,7 +54,14 @@ class PermissionLockdownTest extends TestCase {
 		$maintenanceUser->method( 'getUser' )->willReturn( $maintenanceUserUser );
 		$utilityFactory->method( 'getMaintenanceUser' )->willReturn( $maintenanceUser );
 
-		$handler = new PermissionLockdown( $config, $permissionManager, $utilityFactory, $noSession );
+		$handler = new PermissionLockdown(
+			 $config,
+			 $permissionManager,
+			 $titleFactory,
+			 $revisionLookup,
+			 $utilityFactory,
+			 $noSession
+		);
 
 		$contextTitle = null;
 		$title = $this->createMock( LinkTarget::class );
@@ -87,6 +102,225 @@ class PermissionLockdownTest extends TestCase {
 				'expectedSkip' => false,
 				'userCanReturn' => true,
 				'userCanReturnForMaintenanceuser' => true
+			]
+		];
+	}
+
+	/**
+	 * @param array $params
+	 * @param array $userCanRead
+	 * @param array $expectedModifiedParams
+	 *
+	 * @covers \BlueSpice\HookHandler\PermissionLockdown::onApiCheckCanExecute
+	 *
+	 * @dataProvider provideOnApiCheckCanExecuteTestData
+	 */
+	public function testOnApiCheckCanExecute( $params, $userCanRead, $expectedModifiedParams ) {
+		$config = new HashConfig( [] );
+		$permissionManager = $this->createMock( PermissionManager::class );
+		$permissionManager->method( 'userCan' )->willReturnCallback(
+			static function ( $action, $user, $title ) use ( $userCanRead ) {
+				return $userCanRead[$title->getPrefixedText()] ?? false;
+			} );
+		$titleFactoryReal = MediaWikiServices::getInstance()->getTitleFactory();
+		$titleFactory = $this->createMock( TitleFactory::class );
+		$titleFactory->method( 'newFromText' )->willReturnCallback(
+			static function ( $text ) use ( $titleFactoryReal ) {
+				$title = $titleFactoryReal->newFromText( $text );
+				return $title;
+			} );
+		$titleFactory->method( 'newFromPageIdentity' )->willReturnCallback(
+			static function ( $pageIdentity ) use ( $titleFactoryReal ) {
+				$title = $titleFactoryReal->newFromPageIdentity( $pageIdentity );
+				return $title;
+			} );
+		$titleFactory->method( 'newFromID' )->willReturnCallback(
+			static function ( $id ) use ( $titleFactoryReal ) {
+				// For the sake of this test, we align the page name with the page ID
+				$pageName = "Page$id";
+				$title = $titleFactoryReal->newFromText( $pageName );
+				return $title;
+			} );
+		$revisionLookup = $this->createMock( RevisionLookup::class );
+		$me = $this;
+		$revisionLookup->method( 'getRevisionById' )->willReturnCallback(
+			static function ( $id ) use ( $me ) {
+				$revision = $me->createMock( RevisionRecord::class );
+				$revision->method( 'getPage' )->willReturnCallback(
+					static function () use ( $id ) {
+						// For the sake of this test, we align the page name with the revision ID
+						$pageName = "Page$id";
+						$title = MediaWikiServices::getInstance()->getTitleFactory()->newFromText( $pageName );
+						return $title;
+					} );
+				return $revision;
+			} );
+		$utilityFactory = $this->createMock( UtilityFactory::class );
+		$noSession = false;
+		$handler = new PermissionLockdown(
+			$config,
+			$permissionManager,
+			$titleFactory,
+			$revisionLookup,
+			$utilityFactory,
+			$noSession
+		);
+
+		$module = $this->createMock( ApiBase::class );
+		$user = $this->createMock( User::class );
+		$request = $this->createMock( WebRequest::class );
+		$request->method( 'getVal' )->willReturnCallback(
+			static function ( $key ) use ( &$params ) {
+				return $params[$key] ?? null;
+			} );
+		$request->method( 'setVal' )->willReturnCallback(
+			static function ( $key, $value ) use ( &$params ) {
+				$params[$key] = $value;
+			} );
+		$module->method( 'getUser' )->willReturn( $user );
+		$module->method( 'getRequest' )->willReturn( $request );
+
+		$handler->onApiBeforeMain( $module );
+		$this->assertEquals( $expectedModifiedParams, $params );
+	}
+
+	public function provideOnApiCheckCanExecuteTestData(): array {
+		return [
+			'titles-changed' => [
+				'params' => [
+					'action' => 'query',
+					'format' => 'json',
+					'prop' => 'info|extracts|pageimages|revisions|info',
+					'formatversion' => '2',
+					'redirects' => 'true',
+					'exchars' => '525',
+					'explaintext' => 'true',
+					'exsectionformat' => 'plain',
+					'piprop' => 'thumbnail',
+					'pithumbsize' => '480',
+					'pilicense' => 'any',
+					'rvprop' => 'timestamp',
+					'inprop' => 'url',
+					'titles' => 'Page1|Page2',
+					'smaxage' => '300',
+					'maxage' => '300',
+					'uselang' => 'content',
+				],
+				'userCanRead' => [
+					'Page1' => false,
+					'Page2' => true
+				],
+				'expectedModifiedParams' => [
+					'action' => 'query',
+					'format' => 'json',
+					'prop' => 'info|extracts|pageimages|revisions|info',
+					'formatversion' => '2',
+					'redirects' => 'true',
+					'exchars' => '525',
+					'explaintext' => 'true',
+					'exsectionformat' => 'plain',
+					'piprop' => 'thumbnail',
+					'pithumbsize' => '480',
+					'pilicense' => 'any',
+					'rvprop' => 'timestamp',
+					'inprop' => 'url',
+					'titles' => 'Page2',
+					'smaxage' => '300',
+					'maxage' => '300',
+					'uselang' => 'content',
+				],
+			],
+			'titles-unchanged' => [
+				'params' => [
+					'action' => 'query',
+					'format' => 'json',
+					'prop' => 'info|extracts|pageimages|revisions|info',
+					'formatversion' => '2',
+					'redirects' => 'true',
+					'exchars' => '525',
+					'explaintext' => 'true',
+					'exsectionformat' => 'plain',
+					'piprop' => 'thumbnail',
+					'pithumbsize' => '480',
+					'pilicense' => 'any',
+					'rvprop' => 'timestamp',
+					'inprop' => 'url',
+					'titles' => 'Page1|Page2',
+					'smaxage' => '300',
+					'maxage' => '300',
+					'uselang' => 'content',
+				],
+				'userCanRead' => [
+					'Page1' => true,
+					'Page2' => true
+				],
+				'expectedModifiedParams' => [
+					'action' => 'query',
+					'format' => 'json',
+					'prop' => 'info|extracts|pageimages|revisions|info',
+					'formatversion' => '2',
+					'redirects' => 'true',
+					'exchars' => '525',
+					'explaintext' => 'true',
+					'exsectionformat' => 'plain',
+					'piprop' => 'thumbnail',
+					'pithumbsize' => '480',
+					'pilicense' => 'any',
+					'rvprop' => 'timestamp',
+					'inprop' => 'url',
+					'titles' => 'Page1|Page2',
+					'smaxage' => '300',
+					'maxage' => '300',
+					'uselang' => 'content',
+				],
+			],
+			'revids-changed' => [
+				'params' => [
+					'revids' => '1|2',
+				],
+				'userCanRead' => [
+					'Page1' => false,
+					'Page2' => true
+				],
+				'expectedModifiedParams' => [
+					'revids' => '2',
+				],
+			],
+			'revids-unchanged' => [
+				'params' => [
+					'revids' => '1|2',
+				],
+				'userCanRead' => [
+					'Page1' => true,
+					'Page2' => true
+				],
+				'expectedModifiedParams' => [
+					'revids' => '1|2',
+				],
+			],
+			'pageids-changed' => [
+				'params' => [
+					'pageids' => '1|2',
+				],
+				'userCanRead' => [
+					'Page1' => false,
+					'Page2' => true
+				],
+				'expectedModifiedParams' => [
+					'pageids' => '2',
+				],
+			],
+			'pageids-unchanged' => [
+				'params' => [
+					'pageids' => '1|2',
+				],
+				'userCanRead' => [
+					'Page1' => true,
+					'Page2' => true
+				],
+				'expectedModifiedParams' => [
+					'pageids' => '1|2',
+				],
 			]
 		];
 	}
