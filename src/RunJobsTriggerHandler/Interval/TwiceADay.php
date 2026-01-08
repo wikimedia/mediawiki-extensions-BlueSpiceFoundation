0651ea67bc3f55e5ea1ac00f8a395cfc8d9f4e7c
driedmueller <daniel.riedmueller@hallowelt.com>
2026-01-08 15:06:01 +0100

    Removed RunJobsTriggerHandlerRegistry attribute, RunJobsTriggerHandler and intervals
    ERM44313
    [5.3]
    
    Change-Id: I84de63b58027df4d03f6bafcdb93a3af4624e8b5

--- src/RunJobsTriggerHandler/Interval/TwiceADay.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace BlueSpice\RunJobsTriggerHandler\Interval;
-
-use MWStake\MediaWiki\Component\RunJobsTrigger\Interval\TwiceADay as IntervalTwiceADay;
-
-class TwiceADay extends IntervalTwiceADay {
-
-}
