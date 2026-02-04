fad1dd565b060fe153c9dd9800c76b3108642bbc
hslater <hamish.slater@hallowelt.com>
2026-02-04 19:23:21 +0700

    Deprecate legacy BSApiCategoryTreeStore (bs-category-treestore)
    ERM46271
    
    [5.2.x]
    
    Change-Id: Ib089d4abd30468686230abfd50a23a9307165d94

--- includes/api/BSApiCategoryStore.php
+++ includes/api/BSApiCategoryStore.php
@@ -5,27 +5,7 @@ use MediaWiki\Title\Title;
 /**
  * This class serves as a backend for the category store.
  *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, version 3.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License along
- * with this program; if not, write to the Free Software Foundation, Inc.,
- * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
- *
- * This file is part of BlueSpice MediaWiki
- * For further information visit https://bluespice.com
- *
- * @author     Leonid Verhovskij <verhovskij@hallowelt.com>
- * @author     Markus Glaser <glaser@hallowelt.com>
- * @package    Bluespice_Foundation
- * @copyright  Copyright (C) 2016 Hallo Welt! GmbH, All rights reserved.
- * @license    http://www.gnu.org/copyleft/gpl.html GPL-3.0-only
+ * @deprecated since 1.43; use commonwebapis component; will be removed in 1.47
  */
 class BSApiCategoryStore extends BSApiExtJSStoreBase {
 
