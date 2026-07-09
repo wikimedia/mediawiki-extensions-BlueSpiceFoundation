a690b2e5d3b0af40c8cd13603c0b9d6a001bbeeb
Hamish <hamish.slater@hallowelt.com>
2026-07-10 01:22:58 +0700

    phpcs

--- src/Renderer/GroupImage.php
+++ src/Renderer/GroupImage.php
@@ -77,7 +77,7 @@ class GroupImage extends \BlueSpice\TemplateRenderer {
 	 * @param string $val
 	 * @return string
 	 */
-	protected function render_imagesrc( $val ) {
+	protected function render_imagesrc( $val ) { // phpcs:ignore MediaWiki.NamingConventions.LowerCamelFunctionsName.FunctionName, Generic.Files.LineLength.TooLong
 		return $this->services->getService( 'MWStake.DynamicFileDispatcher.Factory' )->getUrl(
 			'groupimage',
 			[
